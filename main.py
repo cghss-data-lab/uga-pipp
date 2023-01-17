17e335a936c88ab343b7dcd8b920ada00c50c236
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2023-01-17 16:01:37 -0300

    Adding basic main.py to read .env file

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+load_dotenv()
+# pull env vars for auth and create neo4j driver
+NEO4J_AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASS"))
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
+SESSION = NEO4J_DRIVER.session()
+
+NEO4J_DRIVER.close()
+
