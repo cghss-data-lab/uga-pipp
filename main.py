fc0b6b502b27f48d538bff54c9f20f112bbee357
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-02-17 17:15:11 -0500

    Build graph with carnivoreGMPD data

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 import gmpd
 
-
 load_dotenv()
 # pull env vars for auth and create neo4j driver
 NEO4J_AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASS"))
@@ -14,39 +13,33 @@ NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
 
-def add_taxons():
-    keys = gmpd.get_unique_taxons()
-
-    # Get the hosts and label as both taxons and hosts
-    for key in keys[0]:
-        # print(f'CREATE (:Taxon:Host {{species: "{key}"}}')
-        SESSION.run(
-            f'CREATE (:Taxon:Host {{species: "{key}"}})'
-        )
-    
-    # Get the pathogens and label as both taxons and pathogens
-    for key in keys[1]:
-        # print(f'CREATE (:Taxon:Pathogen {{species: "{key}"}}')
-        SESSION.run(
-            f'CREATE (:Taxon:Pathogen {{species: "{key}"}})'
-        )
-    
-
 if __name__ == "__main__":
     gmpd_rows = gmpd.get_rows()
-    add_taxons()
-    # for index, row in enumerate(gmpd_rows):
-         
-            
-        # Get the host species
-        # Get the parasite species
-        # Because they're on the same row, set the relationship as "Parasite infects species"
-        
 
+    for row in gmpd_rows:
 
-    
+        # Iterate over dataset and create separate taxon nodes for hosts and pathogens (with labels)
+        host_node = SESSION.run(
+            'MERGE (n:Taxon:Host {species: $species}) RETURN n',
+            species=row['species']
+        ).single().get('n')
+        pathogen_node = SESSION.run(
+            'MERGE (n:Taxon:Pathogen {species: $ParasiteGMPD}) '
+            'SET n.closeTransmission = $closeTransmission, n.noncloseTransmission = $noncloseTransmission, n.vectorTransmission = $vectorTransmission, n.intermediateTransmission = $intermediateTransmission '
+            'RETURN n',
+            ParasiteGMPD=row["ParasiteGMPD"],
+            closeTransmission=row["close"],
+            noncloseTransmission=row["nonclose"],
+            vectorTransmission=row["vector"],
+            intermediateTransmission = row["intermediate"]
+        ).single().get("n")
 
-# SESSION.run('CREATE (:Message {message: "Hello World"})-[:FROM]->(:Planet {name: "Earth"})')
+        # Create relationship between hosts and pathogens
+        SESSION.run(
+            'MATCH (s:Taxon:Host {species: $species}), (p:Taxon:Pathogen {species: $ParasiteGMPD}) '
+            'MERGE (p)-[:INFECTS]->(s)',
+            species=row["species"],
+            ParasiteGMPD=row["ParasiteGMPD"]
+        )
 
-NEO4J_DRIVER.close()
-
+    NEO4J_DRIVER.close()
