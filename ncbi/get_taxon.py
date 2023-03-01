c0ab0cae691a5bdc2b06d06543c6a243745cc49a
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-01 14:14:52 -0500

    Get taxon metadata
    Copied from build_graph2 in kr2-graph

---
+++ ncbi/get_taxon.py
@@ -0,0 +1,46 @@
+import ncbi
+from bs4 import Tag
+
+
+def get_taxon(ncbi_id):
+    """Request metadata by NCBI taxonomy ID, and return cleaned object"""
+
+    params = {"db": "Taxonomy", "id": ncbi_id}
+    soup = ncbi.api_soup("efetch", params)
+
+    taxon = soup.TaxaSet.Taxon
+
+    taxon_metadata = {
+        "ScientificName": taxon.ScientificName.getText(),
+        "ParentTaxId": taxon.ParentTaxId.getText(),
+        "Rank": taxon.Rank.getText(),
+        "Division": taxon.Division.getText(),
+        "GeneticCode": {"GCId": taxon.GCId.getText(), "GCName": taxon.GCName.getText()},
+        "MitoGeneticCode": {
+            "MGCId": taxon.MGCId.getText(),
+            "MGCName": taxon.MGCName.getText(),
+        },
+        "Lineage": taxon.Lineage.getText(),
+        "CreateDate": taxon.CreateDate.getText(),
+        "UpdateDate": taxon.UpdateDate.getText(),
+        "PubDate": taxon.PubDate.getText(),
+    }
+
+    if taxon.otherNames:
+        taxon["OtherNames"] = (taxon.OtherNames.getText(),)
+
+    # parse lineage
+    lineage_ex = []
+    for taxon in taxon.LineageEx.children:
+        if isinstance(taxon, Tag):
+            lineage_ex.append(
+                {
+                    "TaxId": taxon.TaxId.getText(),
+                    "ScientificName": taxon.ScientificName.getText(),
+                    "Rank": taxon.Rank.getText(),
+                }
+            )
+
+    taxon_metadata["LineageEx"] = lineage_ex
+
+    return taxon_metadata
