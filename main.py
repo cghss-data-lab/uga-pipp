562cb04539faa668296a941befb5ac2dec3d9396
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-01 20:39:19 -0500

    Merge NCBI and GMPD taxons

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 
-import gmpd
+import carnivoreGMPD
+import ncbi
 
 load_dotenv()
 # pull env vars for auth and create neo4j driver
@@ -13,21 +14,39 @@ NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
 
+def db_merge_gmpd_ncbi(host_species, pathogen_species):
+    host_ncbi_id = ncbi.id_search(f"{host_species}")
+    if host_ncbi_id:
+        host_ncbi_metadata = ncbi.get_taxon(host_ncbi_id)
+        host_taxon = {**host_ncbi_metadata, "TaxId": host_ncbi_id}
+        ncbi.merge_taxon(host_taxon, SESSION)
+        time.sleep(0.4)
+
+    pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
+    if pathogen_ncbi_id:
+        pathogen_ncbi_metadata = ncbi.get_taxon(pathogen_ncbi_id)
+        pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
+        ncbi.merge_taxon(pathogen_taxon, SESSION)
+        time.sleep(0.4)
+
 if __name__ == "__main__":
-    gmpd_rows = gmpd.get_rows()
+    gmpd_rows = carnivoreGMPD.get_rows()
 
     for row in gmpd_rows:
+        host_species = row['host_species']
+        pathogen_species = row['pathogen_species']
 
         # Iterate over dataset and create separate taxon nodes for hosts and pathogens (with labels)
         host_node = SESSION.run(
             'MERGE (n:Taxon:Host {species: $species}) RETURN n',
-            species=row['species']
+            species=host_species
         ).single().get('n')
+
         pathogen_node = SESSION.run(
-            'MERGE (n:Taxon:Pathogen {species: $ParasiteGMPD}) '
+            'MERGE (n:Taxon:Pathogen {species: $species}) '
             'SET n.closeTransmission = $closeTransmission, n.noncloseTransmission = $noncloseTransmission, n.vectorTransmission = $vectorTransmission, n.intermediateTransmission = $intermediateTransmission '
             'RETURN n',
-            ParasiteGMPD=row["ParasiteGMPD"],
+            species=pathogen_species,
             closeTransmission=row["close"],
             noncloseTransmission=row["nonclose"],
             vectorTransmission=row["vector"],
@@ -36,10 +55,13 @@ if __name__ == "__main__":
 
         # Create INFECTS relationship between hosts and pathogens
         SESSION.run(
-            'MATCH (h:Taxon:Host {species: $species}), (p:Taxon:Pathogen {species: $ParasiteGMPD}) '
+            'MATCH (h:Taxon:Host {species: $host_species}), (p:Taxon:Pathogen {species: $pathogen_species}) '
             'MERGE (p)-[:INFECTS]->(h)',
-            species=row["species"],
-            ParasiteGMPD=row["ParasiteGMPD"]
+            host_species=host_species,
+            pathogen_species=pathogen_species
         )
+
+        # Merge host and pathogen nodes with NCBI metadata
+        db_merge_gmpd_ncbi(host_species, pathogen_species)
 
     NEO4J_DRIVER.close()
