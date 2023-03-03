b8ebd294c1c1de145cf8005b8145e500dbec20ef
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-02 22:41:01 -0500

    Refactor hard-coded items and error log

--- main.py
+++ main.py
@@ -15,10 +15,17 @@ NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
 
+NOT_FOUND_FILE = "not_found.txt"
+SLEEP_TIME = 0.5 #2 requests/second
+
+def write_to_not_found(message):
+    with open(NOT_FOUND_FILE, "a") as f:
+        f.write(message)
+
 def create_ncbi_taxon(host_species, pathogen_species):
     try:
         # search for the TaxID that matches the host_species name
-        host_ncbi_id = ncbi.id_search(f"{host_species}")
+        host_ncbi_id = ncbi.id_search(host_species)
 
         # if a term exists, then get the taxon metadata for that ID
         # merge the taxon on TaxID
@@ -26,16 +33,14 @@ def create_ncbi_taxon(host_species, pathogen_species):
             host_ncbi_metadata = ncbi.get_taxon(host_ncbi_id)
             host_taxon = {**host_ncbi_metadata, "TaxId":host_ncbi_id}
             ncbi.merge_taxon(host_taxon, SESSION)
-            time.sleep(0.5) #2r/s
+            time.sleep(SLEEP_TIME)
         
         else:
-            with open("not_found.txt", "a") as f:
-                f.write(f"No NCBI ID for host {host_species}: \n")
+            write_to_not_found(f"No NCBI ID for host {host_species}: \n")
 
 
     except Exception as e:
-        with open("not_found.txt", "a") as f:
-            f.write(f"Error getting taxon for host {host_species}: {e} \n")
+        write_to_not_found(f"Error getting taxon for host {host_species}: {e} \n")
 
     try:
         pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
@@ -43,15 +48,13 @@ def create_ncbi_taxon(host_species, pathogen_species):
             pathogen_ncbi_metadata = ncbi.get_taxon(pathogen_ncbi_id)
             pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
             ncbi.merge_taxon(pathogen_taxon, SESSION)
-            time.sleep(0.5)
+            time.sleep(SLEEP_TIME)
 
         else:
-            with open("not_found.txt", "a") as f:
-                f.write(f"No NCBI ID for pathogen {pathogen_species} \n")
+            write_to_not_found(f"No NCBI ID for pathogen {pathogen_species} \n")
 
     except Exception as e:
-        with open("not_found.txt", "a") as f:
-             f.write(f"Error getting taxon for pathogen {pathogen_species}: {e}\n")    
+        write_to_not_found(f"Error getting taxon for pathogen {pathogen_species}: {e}\n")    
 
 
 def label_ncbi_taxon(host_species, pathogen_species):
