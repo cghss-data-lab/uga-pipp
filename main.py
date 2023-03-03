cb1b506ad0a73cb865d0345f7f068726caf53ffd
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-02 22:32:14 -0500

    Load in NCBI and link to GMPD
    had to re-write and re-organize -- needed to start with creating the nodes from NCBI, THEN label them and create the relationships. previous ways (starting with carnivoreGMPD) were inefficient because the metadata wanted is from NCBI and TaxID is a source of truth.

--- main.py
+++ main.py
@@ -8,24 +8,34 @@ import carnivoreGMPD
 import ncbi
 
 load_dotenv()
-# pull env vars for auth and create neo4j driver
+
+# Pull env vars for auth and create neo4j driver
 NEO4J_AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASS"))
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
 
-def db_merge_gmpd_ncbi(host_species, pathogen_species):
+def create_ncbi_taxon(host_species, pathogen_species):
     try:
+        # search for the TaxID that matches the host_species name
         host_ncbi_id = ncbi.id_search(f"{host_species}")
+
+        # if a term exists, then get the taxon metadata for that ID
+        # merge the taxon on TaxID
         if host_ncbi_id:
             host_ncbi_metadata = ncbi.get_taxon(host_ncbi_id)
-            host_taxon = {**host_ncbi_metadata, "TaxId": host_ncbi_id}
+            host_taxon = {**host_ncbi_metadata, "TaxId":host_ncbi_id}
             ncbi.merge_taxon(host_taxon, SESSION)
-            time.sleep(0.4) 
+            time.sleep(0.5) #2r/s
+        
+        else:
+            with open("not_found.txt", "a") as f:
+                f.write(f"No NCBI ID for host {host_species}: \n")
+
 
     except Exception as e:
         with open("not_found.txt", "a") as f:
-            f.write(f"Error getting taxon for host {host_species}: {e}\n")
+            f.write(f"Error getting taxon for host {host_species}: {e} \n")
 
     try:
         pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
@@ -33,53 +43,52 @@ def db_merge_gmpd_ncbi(host_species, pathogen_species):
             pathogen_ncbi_metadata = ncbi.get_taxon(pathogen_ncbi_id)
             pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
             ncbi.merge_taxon(pathogen_taxon, SESSION)
-            time.sleep(0.4)
-        
+            time.sleep(0.5)
+
+        else:
+            with open("not_found.txt", "a") as f:
+                f.write(f"No NCBI ID for pathogen {pathogen_species} \n")
+
     except Exception as e:
         with open("not_found.txt", "a") as f:
-             f.write(f"Error getting taxon for pathogen {pathogen_species}: {e}\n")           
+             f.write(f"Error getting taxon for pathogen {pathogen_species}: {e}\n")    
+
+
+def label_ncbi_taxon(host_species, pathogen_species):
+    host_query = (
+        "MERGE (t:Taxon {name: $host_species})"
+        "SET t:Host"
+    )
+    SESSION.run(host_query, host_species=host_species)
+
+    pathogen_query = (
+        "MERGE (t:Taxon {name: $pathogen_species})"
+        "SET t:Pathogen"
+    )
+    SESSION.run(pathogen_query, pathogen_species=pathogen_species)
+
+def link_host_pathogen(host_species, pathogen_species):
+    pairings_query = (
+        "MATCH (h:Host {name: $host_species}), (p:Pathogen {name: $pathogen_species}) "
+        "MERGE (p)-[:INFECTS]->(h) "
+    )
+    SESSION.run(pairings_query, host_species=host_species, pathogen_species=pathogen_species)
 
 if __name__ == "__main__":
     gmpd_rows = carnivoreGMPD.get_rows()
 
     for row in gmpd_rows:
+
         host_species = row['host_species']
         pathogen_species = row['pathogen_species']
 
-        # Iterate over dataset and create separate taxon nodes for hosts and pathogens (with labels)
-        host_node = SESSION.run(
-            'MERGE (n:Taxon:Host:Species {name: $species}) '
-            'SET n.Rank = "Species" '
-            'RETURN n',
-            species=host_species
-        ).single().get('n')
+        # Create taxa from NCBI
+        create_ncbi_taxon(host_species, pathogen_species)
 
-        pathogen_node = SESSION.run(
-            'MERGE (n:Taxon:Pathogen:Species {name: $species}) '
-            'SET n.Rank = "Species", n.closeTransmission = $closeTransmission, n.noncloseTransmission = $noncloseTransmission, n.vectorTransmission = $vectorTransmission, n.intermediateTransmission = $intermediateTransmission '
-            'RETURN n',
-            species=pathogen_species,
-            closeTransmission=row["close"],
-            noncloseTransmission=row["nonclose"],
-            vectorTransmission=row["vector"],
-            intermediateTransmission = row["intermediate"]
-        ).single().get("n")
+        # Label the NCBI taxon nodes
+        label_ncbi_taxon(host_species, pathogen_species)
 
-    # SESSION.run(
-    #     f'MERGE (n:Taxon:{rank} {{name: "{taxon["ScientificName"]}", '
-    #     f'  Rank: "{rank}", '
-    #     f'  TaxId: "{taxon["TaxId"]}" '
-    #     f"}})"
-
-        # Create INFECTS relationship between hosts and pathogens
-        SESSION.run(
-            'MATCH (h:Taxon:Host {name: $host_species}), (p:Taxon:Pathogen {name: $pathogen_species}) '
-            'MERGE (p)-[:INFECTS]->(h)',
-            host_species=host_species,
-            pathogen_species=pathogen_species
-        )
-
-        # Merge host and pathogen nodes with NCBI metadata
-        db_merge_gmpd_ncbi(host_species, pathogen_species)
+        # Create pairings
+        link_host_pathogen(host_species, pathogen_species)
 
     NEO4J_DRIVER.close()
