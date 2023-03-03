4984fa24accdc489e0df7cf677557e366f041d61
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-02 23:10:21 -0500

    Initialize set before id_search
    Increase efficiency by not checking the same taxon twice

--- main.py
+++ main.py
@@ -15,12 +15,25 @@ NEO4J_DRIVER = GraphDatabase.driver(NEO4J_URI, auth=NEO4J_AUTH)
 SESSION = NEO4J_DRIVER.session()
 
 if __name__ == "__main__":
+    tax_list = set()
     gmpd_rows = carnivoreGMPD.get_rows()
 
     for row in gmpd_rows:
 
         host_species = row['host_species']
         pathogen_species = row['pathogen_species']
+
+        # Check if host or pathogen has been processed yet
+        if host_species in tax_list:
+            print(f"{host_species} has already been checked")
+            continue
+        elif pathogen_species in tax_list:
+            print(f"{pathogen_species} has already been checked")
+            continue
+        else:
+            # Add the species to the set
+            tax_list.add(host_species)
+            tax_list.add(pathogen_species)
 
         # Create taxa from NCBI
         link_ncbi_gmpd.create_ncbi_taxon(host_species, pathogen_species, SESSION)
