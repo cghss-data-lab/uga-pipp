cf01376a7150ef7cf3e20276b9d564c41fc30e18
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-14 16:49:23 -0400

    Add synonyms for terms

--- gmpd/link_gmpd_to_ncbi.py
+++ gmpd/link_gmpd_to_ncbi.py
@@ -8,11 +8,12 @@ def write_to_not_found(message):
     with open(NOT_FOUND_FILE, "a") as f:
         f.write(message)
 
-# Search for each of the host/path terms in GMPD
-# If there's a term that matches in NCBI, create/merge taxon
-def create_ncbi_taxon(host_species, pathogen_species, SESSION):
+def create_ncbi_taxon(row, SESSION):
     global searched_terms
     try:
+        host_species = row["HostReportedName"]
+        pathogen_species = row["ParasiteReportedName"]
+        
         if host_species not in searched_terms:
             # search for the TaxID that matches the host_reported name
             host_ncbi_id = ncbi.id_search(host_species)
@@ -25,14 +26,24 @@ def create_ncbi_taxon(host_species, pathogen_species, SESSION):
                 host_taxon = {**host_ncbi_metadata, "TaxId":host_ncbi_id}
                 ncbi.merge_taxon(host_taxon, SESSION)
 
-            # elif host_ncbi_id still none
-            # id_search for CorrectedName host_species_corrected
+            elif row["HostCorrectedName"] not in searched_terms:
+                # if the reported name doesn't match, try the corrected name
+                host_ncbi_id = ncbi.id_search(row["HostCorrectedName"])
+                searched_terms.add(row["HostCorrectedName"])
+
+                if host_ncbi_id:
+                    host_ncbi_metadata = ncbi.get_metadata(host_ncbi_id)
+                    host_taxon = {**host_ncbi_metadata, "TaxId":host_ncbi_id}
+                    ncbi.merge_taxon(host_taxon, SESSION)
+
+                else:
+                    write_to_not_found(f"No NCBI ID for host {host_species} or {row['HostCorrectedName']}: \n")
 
             else:
-                write_to_not_found(f"No NCBI ID for host {host_species}: \n")
+                pass
 
         if pathogen_species not in searched_terms:
-            pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
+            pathogen_ncbi_id = ncbi.id_search(pathogen_species)
             searched_terms.add(pathogen_species)
 
             if pathogen_ncbi_id:
@@ -40,11 +51,20 @@ def create_ncbi_taxon(host_species, pathogen_species, SESSION):
                 pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
                 ncbi.merge_taxon(pathogen_taxon, SESSION)
 
-            # elif pathogen_ncbi_id still none
-            # id_search for CorrectedName pathogen_species_corrected
+            elif row["ParasiteCorrectedName"] not in searched_terms:
+                pathogen_ncbi_id = ncbi.id_search(row["ParasiteCorrectedName"])
+                searched_terms.add(row["ParasiteCorrectedName"])
+
+                if pathogen_ncbi_id:
+                    pathogen_ncbi_metadata = ncbi.get_metadata(pathogen_ncbi_id)
+                    pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
+                    ncbi.merge_taxon(pathogen_taxon, SESSION)
+
+                else:
+                    write_to_not_found(f"No NCBI ID for pathogen {pathogen_species} or {row['ParasiteCorrectedName']} \n")
 
             else:
-                write_to_not_found(f"No NCBI ID for pathogen {pathogen_species} \n")
+                pass
 
     except Exception as e:
         write_to_not_found(f"Error getting taxon: {e}\n")
