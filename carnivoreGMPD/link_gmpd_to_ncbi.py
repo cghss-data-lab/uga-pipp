e32845cabcdfb4540fe3566a5fa1dc8462b6c314
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-14 10:47:21 -0400

    keep track of terms

--- carnivoreGMPD/link_gmpd_to_ncbi.py
+++ carnivoreGMPD/link_gmpd_to_ncbi.py
@@ -1,40 +1,45 @@
 import ncbi
-import time
 
 NOT_FOUND_FILE = "carnivoreGMPD/species_not_found.txt"
+
+searched_terms = set() #keep track of terms that have been searched for
 
 def write_to_not_found(message):
     with open(NOT_FOUND_FILE, "a") as f:
         f.write(message)
 
 def create_ncbi_taxon(host_species, pathogen_species, SESSION):
-    looked_up = set()
-    
-    def lookup_taxon(ncbi_id):
-        if ncbi_id and ncbi_id not in looked_up:
-            looked_up.add(ncbi_id)
-            taxon_metadata = ncbi.get_metadata(ncbi_id)
-            taxon = {**taxon_metadata, "TaxId":ncbi_id}
-            ncbi.merge_taxon(taxon, SESSION)
+    global searched_terms
+    try:
+        if host_species not in searched_terms:
+            # search for the TaxID that matches the host_species name
+            host_ncbi_id = ncbi.id_search(host_species)
+            searched_terms.add(host_species)
+
+            # if a term exists, then get the taxon metadata for that ID
+            # merge the taxon on TaxID
+            if host_ncbi_id:
+                host_ncbi_metadata = ncbi.get_metadata(host_ncbi_id)
+                host_taxon = {**host_ncbi_metadata, "TaxId":host_ncbi_id}
+                ncbi.merge_taxon(host_taxon, SESSION)
         
-        elif not ncbi_id:
-            write_to_not_found(f"No NCBI ID for species: {pathogen_species} \n")
-    
-    try:
-        # search for the TaxID that matches the host_species name
-        host_ncbi_id = ncbi.id_search(host_species)
-        lookup_taxon(host_ncbi_id)
+            else:
+                write_to_not_found(f"No NCBI ID for host {host_species}: \n")
+
+        if pathogen_species not in searched_terms:
+            pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
+            searched_terms.add(pathogen_species)
+
+            if pathogen_ncbi_id:
+                pathogen_ncbi_metadata = ncbi.get_metadata(pathogen_ncbi_id)
+                pathogen_taxon = {**pathogen_ncbi_metadata, "TaxId": pathogen_ncbi_id}
+                ncbi.merge_taxon(pathogen_taxon, SESSION)
+
+            else:
+                write_to_not_found(f"No NCBI ID for pathogen {pathogen_species} \n")
 
     except Exception as e:
-        write_to_not_found(f"Error getting taxon for host {host_species}: {e} \n")
-
-    try:
-        pathogen_ncbi_id = ncbi.id_search(f"{pathogen_species}")
-        lookup_taxon(pathogen_ncbi_id)
-
-    except Exception as e:
-        write_to_not_found(f"Error getting taxon for pathogen {pathogen_species}: {e}\n")
-
+        write_to_not_found(f"Error getting taxon: {e}\n")
 
 def link_host_pathogen(host_species, pathogen_species, SESSION):
     pairings_query = (
