02239017c247c145547f8000abea88d09880740f
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-14 18:08:44 -0400

    Handle encoding issue

--- gmpd/get_rows.py
+++ gmpd/get_rows.py
@@ -2,7 +2,7 @@ import csv
 
 def get_rows():
     rows = []
-    with open("gmpd/data/GMPD_main.csv") as gmpd_file:
+    with open("gmpd/data/GMPD_main.csv", encoding='utf-8') as gmpd_file:
         rows = list(csv.DictReader(gmpd_file))
 
     return rows
