1e60217167466bf3e37ff7708fefc9c09ae94d0c
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-13 22:42:42 -0400

    Match to column headers

--- flunet/create_country.py
+++ flunet/create_country.py
@@ -1,24 +1,10 @@
-import logging
+from loguru import logger
 
-logger = logging.getLogger(__name__)
 
-def create_country(country_name, zone_name, session):
+def create_transmission_zone(zone, SESSION):
     """
-    For now, just merges the country into its zone,
-    but this should be expanded to use geonames.
+    For now just creates a TransmissionZone node alone, but
+    should be expanded to tie that node into the GeoNames system
     """
-    logger.info(f"Creating country node ({country_name})")
-
-    query = """
-        MATCH (zone:TransmissionZone {name: $zone_name})
-        CREATE (n:Country:Geo {name: $country_name})-[:IN]->(zone)
-    """
-
-    parameters = {"country_name": country_name, "zone_name": zone_name}
-
-    try:
-        result = session.run(query, parameters)
-        logger.debug(f"Neo4j query result: {result.summary().counters}")
-        
-    except Exception as e:
-        logger.exception(f"Error running Neo4j query: {e}")
+    logger.info(f" CREATE transmission zone node ({zone})")
+    SESSION.run(f'CREATE (n:TransmissionZone:Geo {{name: "{zone}"}})')
