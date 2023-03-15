e148386b935539c27eeb62d2a0f5d5e323692506
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-15 15:56:28 -0400

    Handle pathogens and host relationships

--- gmpd/ingest_gmpd.py
+++ gmpd/ingest_gmpd.py
@@ -8,31 +8,43 @@ def ingest_gmpd(SESSION):
     # Make sure taxons exist in the database
     # Cast variables for properties
     for row in gmpd_rows:
+
+        citation = row["Citation"]
+        prevalence = row["Prevalence"]
+        collected = row["HostsSampled"]
+
+        # Create the Report node if it doesn't exist, and set its label to GMPD
+        query = """
+        MERGE (r:GMPD:Report {citation:$citation, prevalence:$prevalence, collected:$collected})
+        RETURN r
+        """
+        parameters = {"citation": citation, "prevalence": prevalence, "collected": collected}
+        result = SESSION.run(query, parameters)
+        report_node = result.single()[0]
+
         host_tax_id_returned = gmpd.link_gmpd_to_ncbi(row, SESSION)
         pathogen_tax_id_returned = gmpd.link_gmpd_to_ncbi(row, SESSION)
 
-        if host_tax_id_returned and pathogen_tax_id_returned:
-            host_tax_id, pathogen_tax_id = host_tax_id_returned, pathogen_tax_id_returned
 
-            citation = row["Citation"]
-            prevalence = row["Prevalence"]
-            collected = row["HostsSampled"]
-
-            # Create the Report node if it doesn't exist, and set its label to GMPD
-            query = """
-            MERGE (r:GMPD:Report {citation:$citation, prevalence:$prevalence, collected:$collected})
-            RETURN r
-            """
-            parameters = {"citation": citation, "prevalence": prevalence, "collected": collected}
-            result = SESSION.run(query, parameters)
-            report_node = result.single()[0]
+        if host_tax_id_returned:
+            host_tax_id = host_tax_id_returned
 
             # Create the relationships between the Report node and the host / pathogen taxons
             query = """
-            MATCH (r:GMPD:Report), (h:Taxon {TaxId: $host_tax_id}), (p:Taxon {TaxId: $pathogen_tax_id})
+            MATCH (r:GMPD:Report), (h:Taxon {TaxId: $host_tax_id})
             MERGE (r)-[hr:REPORTS {host: 1}]->(h)
+            """
+            parameters = {"host_tax_id": host_tax_id}
+            result = SESSION.run(query, parameters)
+
+        if pathogen_tax_id_returned:
+            pathogen_tax_id = pathogen_tax_id_returned
+
+            # Create the relationships between the Report node and the host / pathogen taxons
+            query = """
+            MATCH (r:GMPD:Report), (p:Taxon {TaxId: $pathogen_tax_id})
             MERGE (r)-[pr:REPORTS {pathogen: 1}]->(p)
             """
-            parameters = {"host_tax_id": host_tax_id, "pathogen_tax_id": pathogen_tax_id}
+            parameters = {"pathogen_tax_id": pathogen_tax_id}
             result = SESSION.run(query, parameters)
 
