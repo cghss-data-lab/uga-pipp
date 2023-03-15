4e4221e231dae827b45e585c367312abbcaf0101
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-15 16:05:40 -0400

    clarify variable names

--- gmpd/ingest_gmpd.py
+++ gmpd/ingest_gmpd.py
@@ -13,6 +13,7 @@ def ingest_gmpd(SESSION):
         prevalence = row["Prevalence"]
         collected = row["HostsSampled"]
         sampleType = row["SamplingType"]
+        
 
         # Create the Report node if it doesn't exist, and set its label to GMPD
         query = """
@@ -23,29 +24,27 @@ def ingest_gmpd(SESSION):
         result = SESSION.run(query, parameters)
         report_node = result.single()[0]
 
-        host_tax_id_returned = gmpd.link_gmpd_to_ncbi(row, SESSION)
-        pathogen_tax_id_returned = gmpd.link_gmpd_to_ncbi(row, SESSION)
+        host_ncbi_id = gmpd.link_gmpd_to_ncbi(row, SESSION)
+        pathogen_ncbi_id = gmpd.link_gmpd_to_ncbi(row, SESSION)
 
 
-        if host_tax_id_returned:
-            host_tax_id = host_tax_id_returned
+        if host_ncbi_id:
 
-            # Create the relationships between the Report node and the host / pathogen taxons
+            # Create the relationships between the Report node and the host taxon
             query = """
-            MATCH (r:GMPD:Report), (h:Taxon {TaxId: $host_tax_id})
+            MATCH (r:GMPD:Report), (h:Taxon {TaxId: $host_ncbi_id})
             MERGE (r)-[hr:REPORTS {host: 1}]->(h)
             """
-            parameters = {"host_tax_id": host_tax_id}
+            parameters = {"host_tax_id": host_ncbi_id}
             result = SESSION.run(query, parameters)
 
-        if pathogen_tax_id_returned:
-            pathogen_tax_id = pathogen_tax_id_returned
+        if pathogen_ncbi_id:
 
-            # Create the relationships between the Report node and the host / pathogen taxons
+            # Create the relationships between the Report node and the pathogen taxon
             query = """
-            MATCH (r:GMPD:Report), (p:Taxon {TaxId: $pathogen_tax_id})
+            MATCH (r:GMPD:Report), (p:Taxon {TaxId: $pathogen_ncbi_id})
             MERGE (r)-[pr:REPORTS {pathogen: 1}]->(p)
             """
-            parameters = {"pathogen_tax_id": pathogen_tax_id}
+            parameters = {"pathogen_tax_id": pathogen_ncbi_id}
             result = SESSION.run(query, parameters)
 
