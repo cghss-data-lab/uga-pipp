594b9b7fa94a278791fd178488ebdfdc60ebfcd3
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-16 13:07:42 -0400

    change name

--- gmpd/ingest_gmpd.py
+++ gmpd/ingest_gmpd.py
@@ -7,7 +7,7 @@ def ingest_gmpd(SESSION):
 
     # Make sure taxons exist in the database
     # Cast variables for properties
-    for GMPDrow, row in enumerate(gmpd_rows):
+    for gmpdRow, row in enumerate(gmpd_rows):
 
         citation = row["Citation"]
         prevalence = row["Prevalence"]
@@ -16,10 +16,10 @@ def ingest_gmpd(SESSION):
 
         # Create the Report node if it doesn't exist, and set its label to GMPD
         query = """
-        MERGE (r:GMPD:Report {citation:$citation, prevalence:$prevalence, collected:$collected, sampleType:$sampleType, GMPDrow:$GMPDrow})
+        MERGE (r:GMPD:Report {citation:$citation, prevalence:$prevalence, collected:$collected, sampleType:$sampleType, gmpdRow:$gmpdRow})
         RETURN r
         """
-        parameters = {"citation": citation, "prevalence": prevalence, "collected": collected, "sampleType": sampleType, "GMPDrow": GMPDrow}
+        parameters = {"citation": citation, "prevalence": prevalence, "collected": collected, "sampleType": sampleType, "gmpdRow": gmpdRow}
         result = SESSION.run(query, parameters)
         report_node = result.single()[0]
 
@@ -29,19 +29,19 @@ def ingest_gmpd(SESSION):
 
             # Create the relationships between the Report node and the host taxon
             query = """
-            MATCH (r:GMPD:Report {GMPDrow: $GMPDrow}), (h:Taxon {TaxId: $host_ncbi_id})
+            MATCH (r:GMPD:Report {gmpdRow: $gmpdRow}), (h:Taxon {TaxId: $host_ncbi_id})
             MERGE (r)-[hr:REPORTS {host: 1}]->(h)
             """
-            parameters = {"GMPDrow": GMPDrow,"host_ncbi_id": host_ncbi_id}
+            parameters = {"gmpdRow": gmpdRow,"host_ncbi_id": host_ncbi_id}
             result = SESSION.run(query, parameters)
 
         if pathogen_ncbi_id:
 
             # Create the relationships between the Report node and the pathogen taxon
             query = """
-            MATCH (r:GMPD:Report {GMPDrow: $GMPDrow}), (p:Taxon {TaxId: $pathogen_ncbi_id})
+            MATCH (r:GMPD:Report {gmpdRow: $gmpdRow}), (p:Taxon {TaxId: $pathogen_ncbi_id})
             MERGE (r)-[pr:REPORTS {pathogen: 1}]->(p)
             """
-            parameters = {"GMPDrow": GMPDrow, "pathogen_ncbi_id": pathogen_ncbi_id}
+            parameters = {"gmpdRow": gmpdRow, "pathogen_ncbi_id": pathogen_ncbi_id}
             result = SESSION.run(query, parameters)
 
