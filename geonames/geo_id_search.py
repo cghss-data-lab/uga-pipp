403c1a88f2568ab7c66f604efeb57f583d569027
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-03-28 16:27:57 -0400

    Change file name to reflect ID search only

---
+++ geonames/geo_id_search.py
@@ -0,0 +1,46 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEO_AUTH = os.getenv("GEO_USER")
+
+def geo_id_search(search_query):
+    """
+    Searches for a location in GeoNames and returns a dictionary with its information.
+    """
+    base_url = "http://api.geonames.org/searchJSON"
+    params = {
+        "q": search_query, 
+        "maxRows": 1, 
+        "username": GEO_AUTH, 
+        "fuzzy":0.8
+        }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if data.get("totalResultsCount") == 0:
+        return None
+
+    geoId = data.get("geonames")[0]["geonameId"]
+    
+    return geoId
+
+   
+    # return {
+    #     "GeoId": result.get("geonameId")
+    # }
+
+        # "name": result.get("name"),
+        # "countryName": result.get("countryName"),
+        # "continent": result.get("continentCode"),
+        # "adminDivision1": result.get("adminCode1"),
+        # "adminDivision2": result.get("adminCode2"),
+        # "adminDivision3": result.get("adminCode3"),
+        # "adminDivision4": result.get("adminCode4"),
+        # "adminDivision5": result.get("adminCode5"),
+        # "latitude": result.get("lat"),
+        # "longitude": result.get("lng"),
+        # "population": result.get("population")
