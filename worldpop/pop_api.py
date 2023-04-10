70dc1106ff3dc243dadb93fc7a7935d43060df50
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-04-10 09:06:07 -0400

    API not working

--- worldpop/pop_api.py
+++ worldpop/pop_api.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 
 
-def pop_api(indicators, locations, params):
-    url = f"https://population.un.org/dataportalapi/api/v1/data/indicators/{indicators}/locations/{locations}&format=json"
-    response = requests.get(url, params)
-    soup = BeautifulSoup(response.content, features="xml")
+# TO DO: Doesn't seem to be publicly advertised, but found this. 
+# Can't figure out how to search by location name and return ID
 
-    return soup
+# def pop_api(indicators, locations, params):
+#     url = f"https://population.un.org/dataportalapi/api/v1/data/indicators/{indicators}/locations/{locations}&format=json"
+#     response = requests.get(url, params)
+#     soup = BeautifulSoup(response.content, features="xml")
+
+#     return soup
