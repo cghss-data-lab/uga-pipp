c6112e3e05d60bc1a7bcd7cc2e8a2c8357e6f165
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-04-11 11:09:04 -0400

    Update comments

--- flunet/create_country.py
+++ flunet/create_country.py
@@ -3,15 +3,15 @@ from geonames import merge_geo
 
 def create_country(country, zone, SESSION):
     """
-    Search for the country in Geonames, obtain its hierarchy,
+    Search for the country by name in Geonames, obtain its hierarchy,
     and create nodes and relationships for each parent.
     """
     logger.info(f"CREATE country node ({country})")
 
-    # Use the geoname ID to get the country's hierarchy
+    # Create country nodes with hierarchy
     merge_geo(country, SESSION)
 
     SESSION.run(
-    f'MATCH  (zone:TransmissionZone {{name: "{zone}"}}) '
-    f'MERGE (g:Geography {{name: "{country}"}})-[:IN]->(zone) '
-)
+        f'MATCH  (zone:TransmissionZone {{name: "{zone}"}}) '
+        f'MERGE (g:Geography {{name: "{country}"}})-[:IN]->(zone) '
+    )
