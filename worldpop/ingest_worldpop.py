aa20af06ea29517a0ff9b4c1cfeec5e6ed28954a
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-04-25 16:56:46 -0400

    Prevent duplicate nodes

--- worldpop/ingest_worldpop.py
+++ worldpop/ingest_worldpop.py
@@ -31,59 +31,57 @@ def ingest_worldpop(SESSION):
                 estimate = False
             raw_date = datetime(year, 7, 1)
             date = raw_date.strftime('%Y-%m-%d')
-            total_pop = (float(row['TPopulation1July']) * 1000)
-            total_male_pop = (float(row['TPopulationMale1July'])*1000)
-            total_female_pop = (float(row['TPopulationFemale1July'])*1000)
-            pop_density_sqkm = float(row["PopDensity"])
-            pop_sex_ratio = float(row["PopSexRatio"])
-            median_age = float(row["MedianAgePop"])
+            totalPopulation = (float(row['TPopulation1July']) * 1000)
+            # totalMalePop = (float(row['TPopulationMale1July'])*1000)
+            # totalFemalePop = (float(row['TPopulationFemale1July'])*1000)
+            popDensity = float(row["PopDensity"])
+            popSexRatio = float(row["PopSexRatio"])
+            medianAge = float(row["MedianAgePop"])
             # nat_change = (float(row["NatChange"])*1000)
-            nat_change_rate = (float(row["NatChangeRT"]))
+            naturalChangeRate = (float(row["NatChangeRT"]))
             # pop_change = (float(row["PopChange"])*1000)
-            pop_growth_rate = (float(row["PopGrowthRate"]))
+            populationGrowthRate = (float(row["PopGrowthRate"]))
             # pop_doubling = (float(row["DoublingTime"]))
             # births = (float(row["Births"])*1000)
-            crude_birth_rate = float(row["CBR"])
-            total_fertility_rate = float(row["TFR"])
-            net_reproduction_rate = float(row["NRR"])
+            crudeBirthRate = float(row["CBR"])
+            totalFertilityRate = float(row["TFR"])
+            netReproductionRate = float(row["NRR"])
             # mean_age_childbearing = float(row["MAC"])
             # deaths = float(row["Deaths"]*1000)
             # male_deaths = float(row["MaleDeaths"]*1000)
             # female_deaths = float(row["FemaleDeaths"]*1000)
-            crude_death_rate = float(row["CDR"])
-            life_expectancy_at_birth = float(row["LEx"])
+            crudeDeathRate = float(row["CDR"])
+            lifeExpectancy = float(row["LEx"])
             # life_expectancy_male = float(row["LExMale"])
             # life_expectancy_female = float(row["LExFemale"])
             # infant_deaths = float(row["InfantDeaths"]*1000)
-            infant_mortality_rate = float(row["IMR"])
+            infantMortalityRate = float(row["IMR"])
             # under_5_deaths = float(row["Under5Deaths"]*1000)
-            under_5_mortality_rate = float(row["Q5"])
+            underFiveMortalityRate = float(row["Q5"])
             # under_40_mortality_rate = float(row["Q0040"])
             # net_migration = float(row["NetMigrations"]*1000)
-            net_migration_rate = float(row["CNMR"])
+            netMigrationRate = float(row["CNMR"])
             duration = 'P1Y' # set duration to 1 year
 
             pop_query = """
                 MERGE (p:Population {dataSource: $dataSource, 
                                 dataSourceRow:$dataSourceRow,
-                                date:date($date),
-                                duration:duration($duration),
-                                totalPopulation:$total_pop, 
-                                totalMalePop:$total_male_pop, 
-                                totalFemalePop: $total_female_pop,  
-                                popDensity:$pop_density_sqkm,
-                                popSexRatio: $pop_sex_ratio,
-                                medianAge: $median_age,
-                                naturalChangeRate: $nat_change_rate,
-                                populationGrowthRate: $pop_growth_rate,
-                                crudeBirthRate: $crude_birth_rate,
-                                totalFertilityRate: $total_fertility_rate,
-                                netReproductionRate: $net_reproduction_rate,
-                                crudeDeathRate: $crude_death_rate,
-                                lifeExpectancy: $life_expectancy_at_birth,
-                                infantMortalityRate: $infant_mortality_rate,
-                                underFiveMoralityRate: $under_5_mortality_rate,
-                                netMigrationRate: $net_migration_rate,
+                                date:$date,
+                                duration:$duration,
+                                totalPopulation:$totalPopulation, 
+                                popDensity:$popDensity,
+                                popSexRatio: $popSexRatio,
+                                medianAge: $medianAge,
+                                naturalChangeRate: $naturalChangeRate,
+                                populationGrowthRate: $populationGrowthRate,
+                                crudeBirthRate: $crudeBirthRate,
+                                totalFertilityRate: $totalFertilityRate,
+                                netReproductionRate: $netReproductionRate,
+                                crudeDeathRate: $crudeDeathRate,
+                                lifeExpectancy: $lifeExpectancy,
+                                infantMortalityRate: $infantMortalityRate,
+                                underFiveMortalityRate: $underFiveMortalityRate,
+                                netMigrationRate: $netMigrationRate,
                                 estimate: $estimate})
                 RETURN p
                 """
@@ -93,27 +91,22 @@ def ingest_worldpop(SESSION):
                 "dataSourceRow":dataSourceRow,
                 "date":date,
                 "duration":duration,
-                "total_pop":total_pop,
-                "total_male_pop":total_male_pop,
-                "total_female_pop":total_female_pop,
-                "pop_density_sqkm":pop_density_sqkm,
-                "pop_sex_ratio":pop_sex_ratio,
-                "median_age":median_age,
-                "nat_change_rate":nat_change_rate,
-                "pop_growth_rate":pop_growth_rate,
-                "crude_birth_rate":crude_birth_rate,
-                "total_fertility_rate":total_fertility_rate,
-                "net_reproduction_rate":net_reproduction_rate,
-                "crude_death_rate":crude_death_rate,
-                "life_expectancy_at_birth":life_expectancy_at_birth,
-                "infant_mortality_rate":infant_mortality_rate,
-                "under_5_mortality_rate":under_5_mortality_rate,
-                "net_migration_rate":net_migration_rate,
+                "totalPopulation":totalPopulation,
+                "popDensity":popDensity,
+                "popSexRatio":popSexRatio,
+                "medianAge":medianAge,
+                "naturalChangeRate":naturalChangeRate,
+                "populationGrowthRate":populationGrowthRate,
+                "crudeBirthRate":crudeBirthRate,
+                "totalFertilityRate":totalFertilityRate,
+                "netReproductionRate":netReproductionRate,
+                "crudeDeathRate":crudeDeathRate,
+                "lifeExpectancy":lifeExpectancy,
+                "infantMortalityRate":infantMortalityRate,
+                "underFiveMortalityRate":underFiveMortalityRate,
+                "netMigrationRate":netMigrationRate,
                 "estimate":estimate
             }
-
-            # Create the Population node
-            SESSION.run(pop_query, parameters)
 
             # Create the GeoNames country if it doesn't exist
             geonames.merge_geo(row["Location"], SESSION)
