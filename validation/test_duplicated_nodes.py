b26a5cd9509a2f425140401f6fa31e47aa2ceac9
David Rosado <drosado@talusanalytics.com>
2023-05-04 15:08:24 -0400

    find nodes with one or multiple labels

--- validation/test_duplicated_nodes.py
+++ validation/test_duplicated_nodes.py
@@ -11,3 +11,10 @@ PASSWORD = os.environ["PASSWORD"]
 DATABASE = os.environ["DATABASE"]
 
 neo4j_connection = Neo4jDatabase(URI, DATABASE, AUTH, PASSWORD)
+
+
+def create_query_node_labels(labels: list) -> str:
+    # Creates a query that returns every memeber of a node type
+    where_clause = " AND ".join(["n:" + l for l in labels])
+    query = f"MATCH (n) WHERE {where_clause} RETURN n"
+    return query
