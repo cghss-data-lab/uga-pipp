e730e44024fa12ef599fbb6db87a7dc01220487e
David Rosado <drosado@talusanalytics.com>
2023-05-04 15:07:18 -0400

    test duplicate nodes

---
+++ validation/test_duplicated_nodes.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from neo4j_driver import Neo4jDatabase
+
+
+load_dotenv()
+
+URI = os.environ["URI"]
+AUTH = os.environ["AUTH"]
+PASSWORD = os.environ["PASSWORD"]
+DATABASE = os.environ["DATABASE"]
+
+neo4j_connection = Neo4jDatabase(URI, DATABASE, AUTH, PASSWORD)
