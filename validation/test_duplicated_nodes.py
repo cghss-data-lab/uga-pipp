87f91499db96634e19d91aa7b8ef531dac1e89da
David Rosado <drosado@talusanalytics.com>
2023-05-04 15:09:16 -0400

    find possible duplicates

--- validation/test_duplicated_nodes.py
+++ validation/test_duplicated_nodes.py
@@ -18,3 +18,18 @@ def create_query_node_labels(labels: list) -> str:
     where_clause = " AND ".join(["n:" + l for l in labels])
     query = f"MATCH (n) WHERE {where_clause} RETURN n"
     return query
+
+
+def test_duplicate_nodes(query: str) -> None:
+    nodes = neo4j_connection.run_query(query)
+    nodes = [dict(node) for node in nodes]
+
+    visited_nodes = set()
+    unique_nodes = []
+    for node in nodes:
+        item = tuple(node.items())
+        if item not in visited_nodes:
+            visited_nodes.add(item)
+            unique_nodes.append(node)
+
+    return unique_nodes, visited_nodes
