e71f918645826f02c9bb71bfbea9c588b8bd027c
David Rosado <drosado@talusanalytics.com>
2023-05-04 15:27:03 -0400

    comments on function function and performance

--- validation/test_duplicated_nodes.py
+++ validation/test_duplicated_nodes.py
@@ -21,15 +21,46 @@ def create_query_node_labels(labels: list) -> str:
 
 
 def test_duplicate_nodes(query: str) -> None:
+    # Visit all nodes and verify if there are duplicates
     nodes = neo4j_connection.run_query(query)
     nodes = [dict(node) for node in nodes]
 
     visited_nodes = set()
     unique_nodes = []
     for node in nodes:
+        # Transoforms the dictionary into a tuple of tuples,
+        # each element is (key, value) from the dictionary
+        # This algorithm might need to sort the nodes before checking. TODO
         item = tuple(node.items())
         if item not in visited_nodes:
             visited_nodes.add(item)
             unique_nodes.append(node)
 
     return unique_nodes, visited_nodes
+
+
+if __name__ == "__main__":
+
+    # Find all node types based on labels
+    node_labels_count = neo4j_connection.run_query(
+        """MATCH (n) RETURN distinct labels(n)"""
+    )
+
+    # From each node type, extract labels and verify if there are duplicates
+    for node in node_labels_count:
+        node_type_labels = dict(node)["labels(n)"]
+        node_type_query = create_query_node_labels(node_type_labels)
+        unique, visited = test_duplicate_nodes(node_type_query)
+
+        test_status = len(unique) == len(visited)
+
+        # Print stauts for each node type
+        print(
+            node_type_labels,
+            "Unique: ",
+            len(unique),
+            "Visited: ",
+            len(visited),
+            "Status: ",
+            test_status,
+        )
