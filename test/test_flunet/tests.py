290e9f644990331987deaffe3059565f6090e910
David Rosado <drosado@talusanalytics.com>
2023-05-10 14:41:40 -0400

    modularize query function

--- test/test_flunet/tests.py
+++ test/test_flunet/tests.py
@@ -9,13 +9,6 @@ with open("./flunet/data/flunet_to_ncbi.csv", "r") as flunet_ncbi:
         flunet_to_ncbi[value] = key
 
 
-def create_query_line_data(row_number: int) -> str:
-    # Query node by row number, return node and first order relationships including nodes
-    # The query should match the row data
-    query = f"MATCH(n)-[r]-(b) WHERE n:FluNet AND n:CaseReport AND n.dataSourceRow = {row_number} RETURN n, r, b, type(r)"
-    return query
-
-
 def is_flunet_node_accurate(row: dict, node: dict) -> bool:
     keys = [
         "",
