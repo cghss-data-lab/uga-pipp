8ba548ef3525d83a838ed48141316053a7f2d13d
David Rosado <drosado@talusanalytics.com>
2023-05-11 16:42:16 -0400

    use pytest to run the tests

--- test/test.py
+++ test/test.py
@@ -1,5 +1,9 @@
-from test_flunet.validation import flunet_validation
+from test_flunet.validation import flunet_validation, flunet_count
 
 
-if __name__ == "__main__":
-    flunet_validation()
+def test_flunet_count():
+    assert flunet_count() == True
+
+
+def test_flunet_accuracy():
+    assert flunet_validation() == 0
