8a199152ede8d0019b65b4adb1eaf17e3d06aa89
David Rosado <drosado@talusanalytics.com>
2023-05-11 16:41:58 -0400

    compare count flunet nodes and csv line data

--- test/test_flunet/validation.py
+++ test/test_flunet/validation.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 from neo4j_driver import Neo4jDatabase
-from .tests import is_line_null, test_flunet_line_data
-from create_query import create_query_line_data
+from .tests import is_line_null, test_flunet_line_data, is_collected_null
+from create_query import create_query_line_data, count_nodes
 from write_logs import write_log
 
 load_dotenv()
@@ -44,19 +44,29 @@ def flunet_validation() -> None:
 
             print("T", total, "E", null, "NE", total - null, end="\r")
     return incorrect
-                with open("./test/logs/flunet_validation.log", "a") as log_file:
-                    incorrect += 1
-                    msg = "ERROR: " + row_as_dictionary[""] + "\n"
-                    log_file.write(msg)
 
-            print("T", total, "E", null, "NE", total - null, end="\r")
-        print(
-            "Total: ",
-            total,
-            "Empty: ",
-            null,
-            "Correct: ",
-            correct,
-            "Incorrect: ",
-            incorrect,
-        )
+
+def flunet_count() -> bool:
+    with open("./flunet/data/flunet_1995_2022.csv", "r") as flunet:
+        header = next(flunet).split(",")
+        null = 0
+        total = 0
+        for row in flunet:
+            row = row.split(",")
+            row_as_dictionary = {k: v for k, v in zip(header, row)}
+            if is_collected_null(row_as_dictionary) or is_line_null(row_as_dictionary):
+                null += 1
+            else:
+                write_log("flunet_count", row_as_dictionary[""])
+                with open("./test/logs/flunet_validation.log", "a") as log_file:
+                    msg = ",".join(row_as_dictionary.values())
+                    log_file.write(msg)
+            total += 1
+
+    count_query = count_nodes("FluNet")
+    result = neo4j_connection.run_query(count_query)
+
+    database_count = dict(result[0])["count(n)"]
+    row_count = total - null
+    print(database_count, row_count)
+    return database_count == row_count
