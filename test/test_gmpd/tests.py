a156dd9c4171a7adab87c91d9dd442b8ca9c8cef
David Rosado <drosado@talusanalytics.com>
2023-05-22 12:44:27 -0400

    determine if count is less than 1

--- test/test_gmpd/tests.py
+++ test/test_gmpd/tests.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from driver.create_query import count_taxons
 
 
 def is_node_accurate(row: dict, node: dict) -> bool:
@@ -14,3 +15,11 @@ def is_node_accurate(row: dict, node: dict) -> bool:
     }
     node.pop("dataSource", None)
     return Counter(list(prunned_row)) == Counter(list(node))
+
+
+def is_node_connected_to_taxa(node: dict) -> bool:
+    result = count_taxons("GMPD", node[""])
+    count = dict(result)["count(t)"]
+    if count < 1:
+        return True
+    return False
