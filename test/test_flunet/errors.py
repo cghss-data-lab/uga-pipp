880c6d0ed920bb299cef9b72729b3933d6cebd3d
David Rosado <drosado@talusanalytics.com>
2023-05-22 16:02:28 -0400

    discrepancy error exception

--- test/test_flunet/errors.py
+++ test/test_flunet/errors.py
@@ -10,3 +10,10 @@ class AccuracyError(Exception):
         self.values = values
         self.message = message
         super().__init__(message)
+
+
+class DiscrepancyError(Exception):
+    def __init__(self, values, message) -> None:
+        self.values = values
+        self.message = message
+        super().__init__(message)
