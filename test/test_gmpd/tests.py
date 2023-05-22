4a25e11b2975e6a6e691a45333791919d5d0539f
David Rosado <drosado@talusanalytics.com>
2023-05-22 12:43:52 -0400

    remove data source if none

--- test/test_gmpd/tests.py
+++ test/test_gmpd/tests.py
@@ -4,10 +4,13 @@ from collections import Counter
 def is_node_accurate(row: dict, node: dict) -> bool:
     prunned_row = {
         "collected": row["NumSamples"],
-        "dataSourceRow": row[""],
+        "dataSourceRow": row[
+            ""
+        ],  # Row is numbered starting at 0, but file starts at 1.
+        # This will be changed so the file starts at 0
         "detectionType": row["SamplingBasis"],
         "prevalence": row["Prevalence"],
         "reference": row["Citation"],
     }
-
+    node.pop("dataSource", None)
     return Counter(list(prunned_row)) == Counter(list(node))
