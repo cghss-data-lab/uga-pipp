eb9347cf64a4fdd53b713b5a691f06bfd49deb03
David Rosado <drosado@talusanalytics.com>
2023-05-22 15:49:32 -0400

    move error new file

---
+++ test/test_flunet/errors.py
@@ -0,0 +1,12 @@
+class ZeroError(Exception):
+    def __init__(self, value, message) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
+class AccuracyError(Exception):
+    def __init__(self, values, message) -> None:
+        self.values = values
+        self.message = message
+        super().__init__(message)
