78fa5ac124b476314c48966df3413873e989f21d
David Rosado <drosado@talusanalytics.com>
2023-05-23 10:28:50 -0400

    flunet test file

---
+++ test/test_flunet/test.py
@@ -0,0 +1,44 @@
+import logging
+from test_flunet.flunet import FluNet, FluNetReport
+from dotenv import load_dotenv
+from test_flunet.errors import (
+    AccuracyError,
+    ZeroError,
+    TerritoryError,
+    DiscrepancyError,
+)
+
+load_dotenv()
+
+
+def validate_flunet(neo4j_driver) -> None:
+    with open("./flunet/data/flunet_1995_2022.csv", "r") as flunet:
+        logging.debug("Flunet main file opened.")
+        header = next(flunet).split(",")
+
+        for row in flunet:
+            # Split data based on csv
+            row = row.split(",")
+            # Create a dictionary with line data
+            row_as_dictionary = {k: v for k, v in zip(header, row)}
+
+            if row_as_dictionary["Collected"] in ("", "0"):
+                logging.warning(f"{row_as_dictionary['']} empty")
+                continue
+
+            try:
+                result = neo4j_driver.run_query()
+                node = FluNet(dict(result))
+                FluNetReport(row_as_dictionary, node)
+
+            except AccuracyError as e:
+                logging.error("Error at %d", row_as_dictionary[""], exc_info=e)
+
+            except ZeroError as e:
+                logging.error("Error at %d", row_as_dictionary[""], exc_info=e)
+
+            except TerritoryError as e:
+                logging.error("Error at %d", row_as_dictionary[""], exc_info=e)
+
+            except DiscrepancyError as e:
+                logging.error("Error at %d", row_as_dictionary[""], exc_info=e)
