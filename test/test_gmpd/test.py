47763053e948553b899b17c43727f57012b23d65
David Rosado <dr997@georgetown.edu>
2023-05-24 12:32:53 -0400

    gmpd validation logic

--- test/test_gmpd/test.py
+++ test/test_gmpd/test.py
@@ -1,6 +1,8 @@
 import logging
-from test_gmpd.types import Gmpd
+from test_gmpd.types import Gmpd, GmpdReport
+from driver.create_query import create_query_line_data
 from driver.errors import DetectionError, AccuracyError, PrevalenceError
+from driver.helpers import split_nodes
 
 
 def validate_gmpd(neo4j_driver) -> None:
@@ -13,9 +15,19 @@ def validate_gmpd(neo4j_driver) -> None:
             row = row.split(",")
             # Create a dictionary with line data
             row_as_dictionary = {k: v for k, v in zip(header, row)}
+            row_number = row_as_dictionary[""]
 
             try:
-                pass
+                query = create_query_line_data("GMPD", row_number)
+                result = neo4j_driver.run_query(query)
+                node, adj_nodes = split_nodes(result)
+                node = Gmpd(dict(node))
+                GmpdReport(
+                    row_data=row_as_dictionary,
+                    neo4j_node=node,
+                    adjacent_nodes=adj_nodes,
+                )
+
             except DetectionError as e:
                 logging.error("Error at %d", "", exc_info=e)
 
