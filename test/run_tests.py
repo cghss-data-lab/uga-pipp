8dec8ee926c7395c7b49448c9b0048eb35ca21d8
David Rosado <dr997@georgetown.edu>
2023-05-24 12:36:03 -0400

    log errors from duplication validation in flunet

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,9 +1,11 @@
 import os
+import logging
 from dotenv import load_dotenv
 from test_flunet.test import validate_flunet
 from test_gmpd.test import validate_gmpd
 from test_global.test import validate_row_duplicates, validate_node_duplicates
 from driver.neo4j_driver import Neo4jDatabase
+from driver.errors import DuplicationError
 
 load_dotenv()
 
@@ -21,8 +23,8 @@ if __name__ == "__main__":
     validate_flunet(neo4j_connection)
     try:
         validate_node_duplicates(neo4j_connection, "FluNet")
-    except Exception as e:
-        print(e)
+    except DuplicationError as e:
+        logging.error("Error in %s", exc_info=e)
 
     validate_gmpd(neo4j_connection)
 
