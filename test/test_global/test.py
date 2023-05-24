92f40bc0009901739dd2b2282abd4ff9c4ff68af
David Rosado <drosado@talusanalytics.com>
2023-05-24 11:32:18 -0400

    fixing minor issues

--- test/test_global/test.py
+++ test/test_global/test.py
@@ -8,9 +8,9 @@ def validate_row_duplicates(neo4j_driver, source: str) -> None:
     """
     query_row_number = count_row_numbers(source)
     row_number_count_result = neo4j_driver.run_query(query_row_number)
-    if row_number_count_result:
+    if not row_number_count_result:
         for row in row_number_count_result:
-            row_number = dict(row)[row]
+            row_number = dict(row)["row"]
             raise DuplicationError(values=row_number, message="Duplicated row number.")
 
 
