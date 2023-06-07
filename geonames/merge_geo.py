e2e1c99563d2ef2ef310da6589d8fd6b8742c0ac
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-06-07 12:42:27 -0400

    cacheing

--- geonames/merge_geo.py
+++ geonames/merge_geo.py
@@ -28,11 +28,9 @@ def get_iso(iso2):
         "maxRows": 1
     }
 
-    # result = result = geo_api("countryInfoJSON", params)
-    # data = result["geonames"][0]["isoAlpha3"]
-
     if iso2 in iso_cache:
         data = iso_cache[iso2]
+
     else:
         result = geo_api("countryInfoJSON", params)
         data = result["geonames"][0]["isoAlpha3"]
