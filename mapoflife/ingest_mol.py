cff9fb48adfa444e62e1743c6538ea7c0ea5fbcf
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-06-14 14:31:07 -0400

    ingest taxa and polygons

--- mapoflife/ingest_mol.py
+++ mapoflife/ingest_mol.py
@@ -29,43 +29,44 @@ def ingest_mol(SESSION):
             # Taxon species :SPANS over the species range (a type of geography node)
             query = """
                 MERGE (t:Taxon {taxId: $taxon_ncbi_id})
-                MERGE (g:Geography:speciesRange {polygon: $wkt_polygon})
+                MERGE (g:Geography:SpeciesRange {polygon: $wkt_polygon})
                 MERGE (t)-[:SPANS {dataSource: "Map of Life", reference: $reference}]->(g)
             """
             logger.info(f'MERGE taxon: {scientificName}')
             SESSION.run(query, params)
 
-            # Spatial query for intersecting points
-            intersects_query = """
-                MATCH (g1:Geography:speciesRange)
-                WHERE exists(g1.polygon)
-                MATCH (g2:Geography)
-                WHERE NOT (g2:speciesRange) AND exists(g2.location)
-                WITH g1, g2, ST_Contains(ST_GeomFromText(g1.polygon), ST_PointFromText(g2.location)) AS contains
-                WHERE contains = true
-                MERGE (g1)-[r:INTERSECTS]->(g2)
-                SET r.intersecting_area = ST_Area(ST_Intersection(ST_GeomFromText(g1.polygon), ST_PointFromText(g2.location)))
-            """
-            batch_size = 1000
+            # # Spatial query for intersecting polygons
+            # intersects_query = """
+            #     MATCH (g1:Geography:SpeciesRange)
+            #     WHERE g1.polygon <> "NA"
+            #     MATCH (g2:Geography)
+            #     WHERE g2.polygon <> "NA" AND id(g1) <> id(g2)
+            #     WITH g1, g2, spatial.intersects(g1.polygon, g2.polygon) AS intersects
+            #     WHERE intersects = true
+            #     MERGE (g1)-[r:INTERSECTS]->(g2)
+            #     SET r.intersecting_area = spatial.area(spatial.intersection(g1.polygon, g2.polygon))
+            # """
+            # batch_size = 1000
 
-            # Get the total count of intersecting points
-            count_query = """
-                MATCH (g1:Geography:speciesRange)
-                WHERE exists(g1.polygon)
-                MATCH (g2:Geography)
-                WHERE NOT (g2:speciesRange) AND exists(g2.location)
-                WITH g1, g2, ST_Contains(ST_GeomFromText(g1.polygon), ST_PointFromText(g2.location)) AS contains
-                WHERE contains = true
-                RETURN count(*) AS count
-            """
+            # count_query = """
+            #     MATCH (g1:Geography:SpeciesRange)
+            #     WHERE g1.polygon <> "NA"
+            #     MATCH (g2:Geography)
+            #     WHERE g2.polygon <> "NA" AND id(g1) <> id(g2)
+            #     WITH g1, g2, spatial.intersects(g1.polygon, g2.polygon) AS intersects
+            #     WHERE intersects = true
+            #     RETURN count(*) AS count
+            # """
 
-            result = SESSION.run(count_query)
-            total_count = result.single()["count"]
+            # result = SESSION.run(count_query)
 
-            # Execute the query in batches
-            for offset in range(0, total_count, batch_size):
-                batch_query = intersects_query + f"\nSKIP {offset} LIMIT {batch_size}"
-                SESSION.run(batch_query)
+
+            # total_count = result.single()["count"]
+
+            # # Execute the query in batches
+            # for offset in range(0, total_count, batch_size):
+            #     batch_query = intersects_query + f"\nSKIP {offset} LIMIT {batch_size}"
+            #     SESSION.run(batch_query)
 
 
 
