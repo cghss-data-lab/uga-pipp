a10150e3928f5ec5b6edde58048b25177d7ba9ce
David Rosado <dr997@georgetown.edu>
2023-06-29 11:15:24 -0400

    queries for viz

--- analysis/embeddings/visualizations.py
+++ analysis/embeddings/visualizations.py
@@ -7,6 +7,8 @@ from neo4j import GraphDatabase
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+import seaborn as sns
 
 load_dotenv()
 
@@ -18,3 +20,25 @@ DATABASE = os.environ["DATABASE"]
 
 
 driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
+label_encoder = preprocessing.LabelEncoder()
+
+with driver.session(database=DATABASE) as session:
+    result = session.run(
+        "MATCH (s)-[r]->(t) RETURN id(s) as source,  id(t) as target, type(r) as relationship"
+    )
+    data = pd.DataFrame([r.values() for r in result], columns=result.keys())
+
+data = np.concatenate([data.source.values, data.target.values])
+label_encoder.fit(data)
+
+
+with driver.session(database=DATABASE) as session:
+    geography = session.run("MATCH (n:Geography)<-[r]->(t) Return id(n)")
+    taxon = session.run("MATCH (n:Taxon)<-[r]->(t) Return id(n)")
+    gmpd = session.run("MATCH (n:Report)<-[r]->(t) Return id(n)")
+    flunet = session.run("MATCH (n:FluNet)<-[r]->(t) Return id(n)")
+
+    geography = [x.values()[0] for x in geography]
+    taxon = [x.values()[0] for x in taxon]
+    gmpd = [x.values()[0] for x in gmpd]
+    flunet = [x.values()[0] for x in flunet]
