4e4baa88bdf2d070a9bb4c47fe24c324303425c8
David Rosado <dr997@georgetown.edu>
2023-08-31 12:06:18 -0400

    removing search lat long and using geoname api obj

--- wahis/ingest_wahis.py
+++ wahis/ingest_wahis.py
@@ -1,14 +1,14 @@
-from loguru import logger
+import time
 from datetime import datetime
-
-from geonames import search_lat_long
-from geonames import merge_geo
-
-import wahis
-
-from wahis import search_and_merge
-import time 
+from loguru import logger
 import requests
+from geonames.geo_api import GeonamesApi
+from geonames import merge_geo
+import wahis
+from wahis import search_and_merge
+
+
+search_lat_long = GeonamesApi().search_lat_long
 
 
 def ingest_wahis(SESSION):
@@ -16,7 +16,7 @@ def ingest_wahis(SESSION):
         try:
             listId = i
             evolution_list = wahis.get_evolution(listId)
-            
+
             # Check if the evolution_list is empty or None
             if not evolution_list:
                 logger.info(f"No evolution list found for listId {listId}. Skipping...")
@@ -27,32 +27,34 @@ def ingest_wahis(SESSION):
                 reportId = evolution_list[x]
                 uqReportId = "WAHIS-" + str(reportId)
 
-                logger.info(f'GETTING report id {reportId}')
+                logger.info(f"GETTING report id {reportId}")
                 metadata = wahis.get_report(reportId)
 
-                if 'event' not in metadata:
+                if "event" not in metadata:
                     logger.error(f"Report {reportId} does not exist. Skipping...")
                     continue
 
                 # Sections to make it easier
-                event = metadata['event']
-                report = metadata['report']
-                outbreaks = metadata['outbreaks']
+                event = metadata["event"]
+                report = metadata["report"]
+                outbreaks = metadata["outbreaks"]
 
                 # On the report node
-                reported_str = report['reportedOn']
-                reported_strip = datetime.strptime(reported_str, '%Y-%m-%dT%H:%M:%S.%f%z')
-                reported = reported_strip.strftime('%Y-%m-%d')
-                reasonForNotification = event['reason']['translation']
-                eventComment = event['eventComment']
-                if eventComment and eventComment['translation'] is not None:
-                    eventDescription = eventComment['translation'] 
-                elif eventComment and eventComment['original'] is not None:
-                    eventDescription = eventComment['original'] 
-                else: 
-                    eventDescription = 'NA'
+                reported_str = report["reportedOn"]
+                reported_strip = datetime.strptime(
+                    reported_str, "%Y-%m-%dT%H:%M:%S.%f%z"
+                )
+                reported = reported_strip.strftime("%Y-%m-%d")
+                reasonForNotification = event["reason"]["translation"]
+                eventComment = event["eventComment"]
+                if eventComment and eventComment["translation"] is not None:
+                    eventDescription = eventComment["translation"]
+                elif eventComment and eventComment["original"] is not None:
+                    eventDescription = eventComment["original"]
+                else:
+                    eventDescription = "NA"
 
-                iso3 = event['country']['isoCode']
+                iso3 = event["country"]["isoCode"]
 
                 # MERGE a REPORT node
                 report_params = {
@@ -60,7 +62,7 @@ def ingest_wahis(SESSION):
                     "uqReportId": uqReportId,
                     "reportDate": reported,
                     "reasonForReport": reasonForNotification,
-                    "description": eventDescription
+                    "description": eventDescription,
                 }
 
                 report_query = """
@@ -75,60 +77,64 @@ def ingest_wahis(SESSION):
                 """
 
                 # RUN query to create REPORT node
-                logger.info(f'MERGE report ID: {reportId}')
+                logger.info(f"MERGE report ID: {reportId}")
                 report_result = SESSION.run(report_query, report_params)
                 report_node = report_result.single()[0]
 
                 # Set to track processed eventIds
-                processed_event_ids = set()  
+                processed_event_ids = set()
 
                 # For each outbreak event listed in the report, grab metadata
                 for index, key in enumerate(outbreaks):
-                    try: 
+                    try:
                         if i == 3721:
                             if index < 596:
                                 continue
 
                         # EVENT :OCCURS_IN GEO
-                        place = key['location']
-                        long = key['longitude']
-                        lat = key['latitude']
-                        if 'description' in key and key['description'] is not None:
-                            desc = key['description']
+                        place = key["location"]
+                        long = key["longitude"]
+                        lat = key["latitude"]
+                        if "description" in key and key["description"] is not None:
+                            desc = key["description"]
                         else:
-                            desc = 'NA'
+                            desc = "NA"
 
                         # Return the location geonameId if there is one
                         if lat and long:
-                            geonameId = search_lat_long(lat, long)
+                            geonameId = search_lat_long((lat, long))
                             merge_geo(geonameId, SESSION)
                         elif place:
                             merge_geo(geonameId, SESSION)
                         else:
                             geonameId = None  # TODO: Use iso3
 
-                        eventId = key['outbreakId']
+                        eventId = key["outbreakId"]
 
                         # Check if the eventId has already been processed and skip processing duplicates
                         if eventId in processed_event_ids:
-                            continue 
+                            continue
 
-                        # Add eventId to set of processed events 
+                        # Add eventId to set of processed events
                         processed_event_ids.add(eventId)
 
                         outbreak_metadata = wahis.get_outbreak(reportId, eventId)
 
-                        if outbreak_metadata is  None:
+                        if outbreak_metadata is None:
                             continue
 
-                        outbreak_str = outbreak_metadata['outbreak']['startDate']
-                        start_strip = datetime.strptime(outbreak_str, '%Y-%m-%dT%H:%M:%S.%f%z')
-                        outbreakStart = start_strip.strftime('%Y-%m-%d')
+                        outbreak_str = outbreak_metadata["outbreak"]["startDate"]
+                        start_strip = datetime.strptime(
+                            outbreak_str, "%Y-%m-%dT%H:%M:%S.%f%z"
+                        )
+                        outbreakStart = start_strip.strftime("%Y-%m-%d")
 
-                        outbreak_end = outbreak_metadata['outbreak']['endDate']
+                        outbreak_end = outbreak_metadata["outbreak"]["endDate"]
                         if outbreak_end:
-                            end_strip = datetime.strptime(outbreak_end, '%Y-%m-%dT%H:%M:%S.%f%z')
-                            outbreakEnd = end_strip.strftime('%Y-%m-%d')
+                            end_strip = datetime.strptime(
+                                outbreak_end, "%Y-%m-%dT%H:%M:%S.%f%z"
+                            )
+                            outbreakEnd = end_strip.strftime("%Y-%m-%d")
                         else:
                             outbreakEnd = "Ongoing"
 
@@ -137,7 +143,6 @@ def ingest_wahis(SESSION):
                             duration = f"P{dur.days}D"
                         else:
                             duration = "NA"
-
 
                         # MERGE Report and Event nodes so that events with same eventId but different reportIds are merged
                         event_query = """
@@ -159,41 +164,46 @@ def ingest_wahis(SESSION):
                             "outbreakStart": outbreakStart,
                             "outbreakEnd": outbreakEnd,
                             "description": desc,
-                            "duration": duration
+                            "duration": duration,
                         }
 
-
-                        logger.info(f'MERGE event ID: ({eventId})')
+                        logger.info(f"MERGE event ID: ({eventId})")
                         SESSION.run(event_query, event_params)
 
                         # Start getting taxon data
                         # Connect to NCBI using serotype if it's available
                         # Otherwise use pathogen name
-                        subtype = event['subType']
+                        subtype = event["subType"]
                         serotype = "NA"
                         pathogen_ncbi = None
-                        if subtype and 'disease' in subtype:
-                            sero = subtype['disease']
-                            if sero and 'name' in sero:
-                                serotype = sero['name']
-                                pathogen_ncbi = wahis.search_and_merge(serotype, SESSION)
-                        
+                        if subtype and "disease" in subtype:
+                            sero = subtype["disease"]
+                            if sero and "name" in sero:
+                                serotype = sero["name"]
+                                pathogen_ncbi = wahis.search_and_merge(
+                                    serotype, SESSION
+                                )
+
                         pathogen_mapping = {
                             "Equine infectious anaemia virus  ": "Equine infectious anemia virus",
                             "Eastern equine encephalomyelitis and Western equine encephalomyelitis viruses ": "Alphavirus",
                             "Paramyxovirus type 1 (PMV-1)": "Pigeon paramyxovirus 1",
-                            "H5N8":"H5N8 subtype"
+                            "H5N8": "H5N8 subtype",
                         }
 
                         if not pathogen_ncbi:
-                            path = event['causalAgent']
-                            if path and 'name' in path:
-                                pathogen = path['name']
+                            path = event["causalAgent"]
+                            if path and "name" in path:
+                                pathogen = path["name"]
                                 if pathogen in pathogen_mapping:
                                     fixed_path = pathogen_mapping[pathogen]
-                                    pathogen_ncbi = wahis.search_and_merge(fixed_path, SESSION)
+                                    pathogen_ncbi = wahis.search_and_merge(
+                                        fixed_path, SESSION
+                                    )
                                 else:
-                                    pathogen_ncbi = wahis.search_and_merge(pathogen, SESSION)
+                                    pathogen_ncbi = wahis.search_and_merge(
+                                        pathogen, SESSION
+                                    )
 
                         if pathogen_ncbi:
                             pathogen_ncbi_id = int(pathogen_ncbi)
@@ -204,43 +214,43 @@ def ingest_wahis(SESSION):
                         speciesName = None
                         speciesWild = "NA"
 
-                        species_quantities = outbreak_metadata['speciesQuantities']
+                        species_quantities = outbreak_metadata["speciesQuantities"]
                         if species_quantities:
                             # iterate over each host species type and retrieve metadata (name, cases, etc.)
                             for item in species_quantities:
-                                newQuantities = item['newQuantities']
+                                newQuantities = item["newQuantities"]
                                 if newQuantities:
-                                    speciesName = newQuantities['speciesName']
-                                    speciesWild = newQuantities['isWild']
-                                    caseCount = newQuantities['cases']
-                                    deathCount = newQuantities['deaths']
+                                    speciesName = newQuantities["speciesName"]
+                                    speciesWild = newQuantities["isWild"]
+                                    caseCount = newQuantities["cases"]
+                                    deathCount = newQuantities["deaths"]
                         else:
-                            quantData = metadata['quantitativeData']
+                            quantData = metadata["quantitativeData"]
                             if quantData:
-                                newQuantData = quantData['news']
+                                newQuantData = quantData["news"]
                                 if newQuantData:
                                     for key in newQuantData:
-                                        speciesName = key['speciesName']
-                                        speciesWild = key['isWild']
-                                        caseCount = key['cases']
-                                        deathCount = key['deaths']
+                                        speciesName = key["speciesName"]
+                                        speciesWild = key["isWild"]
+                                        caseCount = key["cases"]
+                                        deathCount = key["deaths"]
                             else:
                                 quantData = None
-                                logger.info(f'No quant data for outbreak {eventId} or report {reportId}')
-
-                                        
+                                logger.info(
+                                    f"No quant data for outbreak {eventId} or report {reportId}"
+                                )
 
                         # Check if caseCount and deathCount are available
                         if caseCount is not None and caseCount != "NA":
                             caseCount = int(caseCount)
-                        else: 
+                        else:
                             caseCount = "NA"
 
                         if deathCount is not None and deathCount != "NA":
                             deathCount = int(deathCount)
                         else:
                             deathCount = "NA"
-                        
+
                         # If there's a speciesName for the host, continue with NCBI search
                         if speciesName:
                             host_ncbi = wahis.search_and_merge(speciesName, SESSION)
@@ -279,13 +289,13 @@ def ingest_wahis(SESSION):
                                     "geonameId": geonameId,
                                     "pathogen_ncbi_id": pathogen_ncbi_id,
                                     "detectionDate": outbreakStart,
-                                    "detectionDateDetails":"Outbreak start date",
-                                    "subtype":serotype
+                                    "detectionDateDetails": "Outbreak start date",
+                                    "subtype": serotype,
                                 }
 
-                                logger.info(f'MERGE host: {speciesName}')
+                                logger.info(f"MERGE host: {speciesName}")
                                 SESSION.run(host_query, host_params)
-                    
+
                         # Otherwise, do the pathogen only
                         else:
                             path_query = """
@@ -308,31 +318,28 @@ def ingest_wahis(SESSION):
                                 "pathogen_ncbi_id": pathogen_ncbi_id,
                                 "detectionDate": outbreakStart,
                                 "detectionDateDetails": "Outbreak start date",
-                                "subtype": serotype
+                                "subtype": serotype,
                             }
 
-                            logger.info(f'MERGE pathogen only, taxId: {pathogen_ncbi_id}')
+                            logger.info(
+                                f"MERGE pathogen only, taxId: {pathogen_ncbi_id}"
+                            )
                             SESSION.run(path_query, path_params)
-                    
+
                     except KeyError as e:
-                        logger.error(f"KeyError occurred: {e}. Skipping to next outbreak event.")
+                        logger.error(
+                            f"KeyError occurred: {e}. Skipping to next outbreak event."
+                        )
                         continue
 
         except requests.exceptions.JSONDecodeError as e:
-            logger.error(f'Error decoding JSON response: {e}')
+            logger.error(f"Error decoding JSON response: {e}")
             continue
 
         except Exception as e:
             if eventId is not None:
-                if reportId is not None: 
-                        logger.info(f'Stopped at eventId {eventId} and reportId {reportId}')
+                if reportId is not None:
+                    logger.info(f"Stopped at eventId {eventId} and reportId {reportId}")
 
-            logger.error(f'An exception occurred: {e}')
+            logger.error(f"An exception occurred: {e}")
             raise
-
-
-
-                        
-
-
-            
