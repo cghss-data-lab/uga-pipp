ec73716a1771bfd3711e446970ff271013d47e7f
David Rosado <dr997@georgetown.edu>
2023-09-12 15:50:47 -0400

    implementing multi fstrings and other corrections

--- ncbi/merge_taxon.py
+++ ncbi/merge_taxon.py
@@ -5,7 +5,8 @@ from loguru import logger
 def sanitize_rank(rank):
     return rank.title().replace(" ", "")
 
-def merge_taxon_node(taxon, SESSION):
+
+def merge_taxon_node(taxon, session):
     logger.info(f' MERGE node ({taxon["scientificName"]})')
 
     rank = sanitize_rank(taxon["rank"])
@@ -13,16 +14,18 @@ def merge_taxon_node(taxon, SESSION):
     data_source = "NCBI Taxonomy"
 
     # Use rank, TaxID, dataSource, and name as merge conditions
-    SESSION.run(
-        f'MERGE (n:Taxon:{rank} {{name: "{taxon["scientificName"]}", '
-        f'  rank: "{rank}", '
-        f'  dataSource: "{data_source}", '
-        f'  taxId: {taxon_id} '
-        f"}})"
+    session.run(
+        f"""
+        MERGE (n:Taxon:{rank} {{
+            name: "{taxon["scientificName"]}", 
+            rank: "{rank}",
+            dataSource: "{data_source}",
+            taxId: {taxon_id}}})
+        """
     )
 
 
-def merge_taxon_link(parent, child, SESSION):
+def merge_taxon_link(parent, child, session):
     logger.info(
         f'MERGE link ({parent["scientificName"]})'
         f'-[:CONTAINS_TAXON]->({child["scientificName"]})'
@@ -31,32 +34,34 @@ def merge_taxon_link(parent, child, SESSION):
     parent_taxid = int(parent["taxId"])
     child_taxid = int(child["taxId"])
 
-    SESSION.run(
-        f'MATCH (parent:Taxon {{taxId: {parent_taxid}}}), '
-        f'  (child:Taxon {{taxId: {child_taxid}}}) '
-        f"MERGE (parent)-[:CONTAINS_TAXON]->(child) "
+    session.run(
+        f"""
+        MATCH (parent:Taxon {{taxId: {parent_taxid}}}),
+        (child:Taxon {{taxId: {child_taxid}}})
+        MERGE (parent)-[:CONTAINS_TAXON]->(child)
+        """
     )
 
-def merge_lineage(lineage, SESSION):
-    for index, taxon in enumerate(lineage):
-        # merge parent node
-        merge_taxon_node(taxon, SESSION)
 
+def merge_lineage(lineage, session):
+    for index, taxon in enumerate(lineage, start=1):
+        # merge parent node
+        merge_taxon_node(taxon, session)
         # if there are children
-        if index + 1 < len(lineage):
+        if index < len(lineage):
             child = lineage[index + 1]
             # merge child node
-            merge_taxon_node(child, SESSION)
+            merge_taxon_node(child, session)
             # merge relationship
-            merge_taxon_link(taxon, child, SESSION)
+            merge_taxon_link(taxon, child, session)
 
 
-def merge_taxon(taxon, SESSION):
+def merge_taxon(taxon, session):
     logger.info(f'Merging lineage for {taxon["scientificName"]}')
     # handle connecting taxon to first parent
-    merge_taxon_node(taxon, SESSION)
-    merge_taxon_node(taxon["lineageEx"][-1], SESSION)
-    merge_taxon_link(taxon["lineageEx"][-1], taxon, SESSION)
+    merge_taxon_node(taxon, session)
+    merge_taxon_node(taxon["lineageEx"][-1], session)
+    merge_taxon_link(taxon["lineageEx"][-1], taxon, session)
 
     # merge lineage array
-    merge_lineage(taxon["lineageEx"], SESSION)
+    merge_lineage(taxon["lineageEx"], session)
