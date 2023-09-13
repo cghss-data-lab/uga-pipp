95a8ebc60ed916aed26ff569fb3e68327e9272d0
David Rosado <dr997@georgetown.edu>
2023-09-13 12:25:42 -0400

    in case we need for the future

--- worldpop/pop_api.py
+++ worldpop/pop_api.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# TO DO: Doesn't seem to be publicly advertised, but found this. 
+# TO DO: Doesn't seem to be publicly advertised, but found this.
 # Can't figure out how to search by location name and return ID
+# FYI : https://population.un.org/dataportalapi/index.html
 
 # def pop_api(indicators, locations, params):
 #     url = f"https://population.un.org/dataportalapi/api/v1/data/indicators/{indicators}/locations/{locations}&format=json"
