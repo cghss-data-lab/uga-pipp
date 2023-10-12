46345c2c9b7cebe4f8132de202029bb3010fd007
David Rosado <dr997@georgetown.edu>
2023-10-12 16:56:30 -0400

    flunet builds everything except hierarchies

--- src/flunet/ingest_flunet.py
+++ src/flunet/ingest_flunet.py
@@ -1,38 +1,47 @@
 import asyncio
-from loguru import logger
 from src.flunet.process_flunet import valid_flunet, split_influenza_type
 
 QUERY = "./src/flunet/flunet.cypher"
 
 
-def append_geoname_id(processed: dict, geonames: dict) -> dict:
+def append_geoname(processed: dict, geonames: dict) -> None:
     territory = processed["Territory"]
-    processed["geonameId"] = geonames[territory]
-    return processed
+    processed["geonames"] = geonames[territory]
 
 
 async def ingest_flunet(database_handler, geoapi, batch_size: int = 1000) -> list:
     flunet, geonames, geoids = valid_flunet(geoapi)
     geoids = await asyncio.gather(*geoids)
-    geos = dict(zip(geonames, geoids))
-    mapping = [append_geoname_id(row, geos) for row in flunet]
-    infa, infb = split_influenza_type(mapping)
 
-    geoids = [x for x in geoids if x is not None]
+    geos = dict(zip(geonames, geoids))
+    for row in flunet:
+        append_geoname(row, geos)
+
+    infa, infb = split_influenza_type(flunet)
 
     tasks = []
 
     batches_infa = (len(infa) - 1) // batch_size + 1
     for i in range(batches_infa):
         batch = infa[i * batch_size : (i + 1) * batch_size]
-        tasks.append(database_handler.execute_query(QUERY, properties=batch))
+        tasks.append(await database_handler.execute_query(QUERY, properties=batch))
 
     batches_infb = (len(infb) - 1) // batch_size + 1
     for i in range(batches_infb):
         batch = infa[i * batch_size : (i + 1) * batch_size]
-        tasks.append(database_handler.execute_query(QUERY, properties=batch))
+        tasks.append(await database_handler.execute_query(QUERY, properties=batch))
 
-    tasks.extend([geoapi.search_hierarchy(geoid) for geoid in geoids])
-    tasks.extend([database_handler.merge_geo(geoid) for geoid in geoids])
+    geoids = [x for x in geoids if x is not None]
+    hierarchies = [
+        await geoapi.search_hierarchy(geoid["geonameId"]) for geoid in geoids
+    ]
+    tasks.extend(hierarchies)
+
+    # tasks.extend(
+    #     [
+    #         await database_handler.merge_geo(geoid, hierarchy)
+    #         for geoid, hierarchy in zip(geoids, hierarchies)
+    #     ]
+    # )
 
     return tasks
