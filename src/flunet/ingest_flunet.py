1b0e3fdfc399c31f70d3aa019bf5b7e7d7b00aea
David Rosado <dr997@georgetown.edu>
2023-10-12 10:55:46 -0400

    retrun tasks

--- src/flunet/ingest_flunet.py
+++ src/flunet/ingest_flunet.py
@@ -11,11 +11,28 @@ def append_geoname_id(processed: dict, geonames: dict) -> dict:
     return processed
 
 
-async def ingest_flunet(geoapi, file=DATASET) -> list:
+async def ingest_flunet(database_handler, geoapi, batch_size: int = 1000) -> list:
     flunet, geonames, geoids = valid_flunet(geoapi)
     geoids = await asyncio.gather(*geoids)
     geos = dict(zip(geonames, geoids))
     mapping = [append_geoname_id(row, geos) for row in flunet]
     infa, infb = split_influenza_type(mapping)
 
-    return infa, infb, geoids
+    geoids = [x for x in geoids if x is not None]
+
+    tasks = []
+
+    batches_infa = (len(infa) - 1) // batch_size + 1
+    for i in range(batches_infa):
+        batch = infa[i * batch_size : (i + 1) * batch_size]
+        tasks.append(database_handler.execute_query(QUERY, properties=batch))
+
+    batches_infb = (len(infb) - 1) // batch_size + 1
+    for i in range(batches_infb):
+        batch = infa[i * batch_size : (i + 1) * batch_size]
+        tasks.append(database_handler.execute_query(QUERY, properties=batch))
+
+    tasks.extend([geoapi.search_hierarchy(geoid) for geoid in geoids])
+    tasks.extend([database_handler.merge_geo(geoid) for geoid in geoids])
+
+    return tasks
