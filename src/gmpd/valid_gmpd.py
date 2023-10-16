706a5274975bf2ce8419336ccdf7d293038d4f53
David Rosado <dr997@georgetown.edu>
2023-10-16 12:20:08 -0400

    processing gmpd

--- src/gmpd/valid_gmpd.py
+++ src/gmpd/valid_gmpd.py
@@ -26,7 +26,10 @@ def process_geographies(geographies: str) -> list[str]:
     return re.split(r",\s*|and\s*", geographies)
 
 
-def valid_gmpd(file: str = "/data/GMPD_main.csv") -> list[dict]:
+def valid_gmpd(geoapi, ncbi_api, file: str = "/data/GMPD_main.csv") -> list[dict]:
+    geonames = set()
+    tax_names = set()
+
     with open(file, "r", encoding="utf-8-sig") as gmpd:
         gmpd_valid = []
         header = next(gmpd)
@@ -34,9 +37,22 @@ def valid_gmpd(file: str = "/data/GMPD_main.csv") -> list[dict]:
         for row in gmpd:
             row = extract_row(row.strip())
             data = dict(zip(header, row))
+
             logger.info(f"Processing row {data['dataSourceRow']}")
-            if is_valid_report(data):
-                data["Positive"] = float(data["Prevalence"]) * float(data["NumSamples"])
-                data["LocationName"] = process_geographies(data["LocationName"])
-                gmpd_valid.append(data)
-    return gmpd_valid
+
+            if not is_valid_report(data):
+                continue
+
+            data["Positive"] = float(data["Prevalence"]) * float(data["NumSamples"])
+            locations = process_geographies(data["LocationName"])
+            data["LocationName"] = locations
+
+            geonames.update(locations)
+            tax_names.add(data["HostCorrectedName"])
+
+            gmpd_valid.append(data)
+
+        geonames_id = [geoapi.search_geonameid(territory) for territory in geonames]
+        tax_id = [ncbi_api.search_id(tax) for tax in tax_names]
+
+    return gmpd_valid, geonames, geonames_id, tax_names, tax_id
