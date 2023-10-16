798b7ab8aa63199700a2f9369dd11c6e92f3b6a8
David Rosado <dr997@georgetown.edu>
2023-10-16 15:45:03 -0400

    refactoring hierarchy search

--- network/ncbi_api.py
+++ network/ncbi_api.py
@@ -40,90 +40,27 @@ class NCBIApi:
         return ncbi_id
 
     @cache(METADATA_CACHE_FILE, is_class=True)
-    async def search_metadata(self, ncbi_id, source: str = "NCBI Taxonomy"):
+    async def search_hierarchy(self, ncbi_id, source: str = "NCBI Taxonomy"):
         """Request metadata by NCBI taxonomy ID, and return cleaned object"""
+
+        def extract_metadata(taxon: Tag) -> dict:
+            taxon_metadata = {
+                "taxId": taxon.TaxId.getText(),
+                "name": taxon.ScientificName.getText(),
+                "rank": taxon.Rank.getText(),
+                "dataSource": source,
+            }
+            return taxon_metadata
 
         params = {"db": "Taxonomy", "id": ncbi_id}
         soup = await self._api_soup("efetch", params)
 
-        if not soup.TaxaSet:
-            raise ValueError("TaxaSet object not found in the API response.")
+        taxon = extract_metadata(soup.TaxaSet.Taxon)
 
-        taxon_set = soup.TaxaSet
+        taxon_set = soup.TaxaSet.Taxon.LineageEx.find_all("Taxon")
+        taxon_set = [extract_metadata(taxon) for taxon in taxon_set]
 
-        if not taxon_set.Taxon:
-            raise ValueError("Taxon object not found in the API response.")
-
-        taxon = taxon_set.Taxon
-
-        taxon_metadata = {
-            "scientificName": taxon.ScientificName.getText(),
-            "parentTaxId": taxon.ParentTaxId.getText(),
-            "rank": taxon.Rank.getText(),
-            "division": taxon.Division.getText(),
-            "geneticCode": {
-                "GCId": taxon.GCId.getText(),
-                "GCName": taxon.GCName.getText(),
-            },
-            "mitoGeneticCode": {
-                "MGCId": taxon.MGCId.getText(),
-                "MGCName": taxon.MGCName.getText(),
-            },
-            "lineage": taxon.Lineage.getText(),
-            "createDate": taxon.CreateDate.getText(),
-            "updateDate": taxon.UpdateDate.getText(),
-            "pubDate": taxon.PubDate.getText(),
-            "dataSource": source,
-        }
-
-        if taxon.OtherNames:
-            taxon_metadata["otherNames"] = (taxon.OtherNames.getText(),)
-
-        # parse lineage
-        lineage_ex = []
-        if taxon.LineageEx and taxon.LineageEx.children:
-            for taxon_child in taxon.LineageEx.children:
-                if isinstance(taxon_child, Tag):
-                    lineage_ex.append(
-                        {
-                            "taxId": taxon_child.TaxId.getText(),
-                            "scientificName": taxon_child.ScientificName.getText(),
-                            "rank": taxon_child.Rank.getText(),
-                            "dataSource": source,
-                        }
-                    )
-        taxon_metadata["lineageEx"] = lineage_ex
-
-        return taxon_metadata
-
-    @cache(SUBTREE_CACHE_FILE, is_class=True)
-    async def search_subtree(self, ncbi_id):
-        """Get Subtree from text search, using NCBI esearch eutil"""
-        logger.info(f"Searching NCBI for ID {ncbi_id}")
-
-        term = f"txid{str(ncbi_id)}[Subtree]"
-        params = {"db": "Taxonomy", "term": term}
-        soup = await self._api_soup("esearch", params)
-
-        try:
-            id_list = []
-            id_elements = soup.find_all("Id")
-            for id_element in id_elements:
-                id_list.append(id_element.getText())
-
-            return id_list
-
-        except AttributeError:
-            errors = soup.find("ErrorList")
-            warnings = soup.find("WarningList")
-
-            for error in errors.children:
-                logger.error(f"{error.name}: {error.getText()}")
-
-            for warning in warnings.children:
-                logger.warning(f"{warning.name}: {warning.getText()}")
-
-            return None
+        return taxon_set.append(taxon)
 
     async def _api_soup(self, eutil, parameters):
         """
