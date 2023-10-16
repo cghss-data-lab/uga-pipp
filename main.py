7dd83099014b1331a344d629694bbab3b33293ab
David Rosado <dr997@georgetown.edu>
2023-10-16 12:55:19 -0400

    adding gmpd to main

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from network.neo4j_handler import Neo4jHandler
 from network.geo_api import GeonamesApi
 from network.ncbi_api import NCBIApi
 from src.flunet.ingest_flunet import ingest_flunet
+from src.gmpd.ingest_gmpd import ingest_gmpd
 
 
 async def main() -> None:
     database_handler, geonames_api, ncbi_api = Neo4jHandler(), GeonamesApi(), NCBIApi()
 
-    await ingest_flunet(database_handler, geonames_api, ncbi_api)
+    # await ingest_flunet(database_handler, geonames_api, ncbi_api)
+    await ingest_gmpd(database_handler, geonames_api, ncbi_api)
 
 
 if __name__ == "__main__":
