44331b34cee05a90a59869a2dbf267a0631dc80f
David Rosado <dr997@georgetown.edu>
2023-10-16 16:09:26 -0400

    processing steps before ingestion gmpd

--- src/gmpd/ingest_gmpd.py
+++ src/gmpd/ingest_gmpd.py
@@ -6,6 +6,16 @@ from src.gmpd.valid_gmpd import valid_gmpd
 QUERY = "./src/gmpd/gmpd.cypher"
 
 
+def process_geographies(name: str, geographies: dict) -> dict:
+    geo = {"name": name, "geonameId": geographies[name]}
+    return geo
+
+
+def process_taxon(name: str, taxons: dict) -> dict:
+    tax = {"name": name, "taxId": taxons[name]}
+    return tax
+
+
 async def ingest_gmpd(
     database_handler, geoapi, ncbiapi, batch_size: int = 1000, query_path=QUERY
 ) -> None:
@@ -13,3 +23,12 @@ async def ingest_gmpd(
 
     geoids = await asyncio.gather(*geoids)
     taxids = await asyncio.gather(*taxids)
+
+    geographies = dict(zip(geonames, geoids))
+    taxons = dict(zip(taxnames, taxids))
+
+    for row in gmpd:
+        row["locations"] = [
+            process_geographies(geo, geographies) for geo in row["locations"]
+        ]
+        row["HostCorrectedName"] = process_taxon(row["HostCorrectedName"], taxons)
