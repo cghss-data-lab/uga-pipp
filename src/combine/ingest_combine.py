daed1bae8e068f4dbf414957c513d9e3347b6ba0
David Rosado <dr997@georgetown.edu>
2023-10-18 11:36:24 -0400

    processing combine

--- src/combine/ingest_combine.py
+++ src/combine/ingest_combine.py
@@ -1,2 +1,13 @@
-async def ingest_combine(database_handler):
-    pass
+from network.sempahore import handle_concurrency
+from src.combine.valid_combine import valid_combine
+
+
+async def ingest_combine(
+    database_handler, batch_size: int = 1000, query_path="src/combine/combine.cypher"
+) -> None:
+    combine = valid_combine()
+
+    batches = (len(combine) - 1) // batch_size + 1
+    for i in range(batches):
+        batch = combine[i * batch_size : (i + 1) * batch_size]
+        await database_handler.execute_query(query_path, properties=batch)
