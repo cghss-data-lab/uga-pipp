22594ba8c1fc3d6265888d874eab4ec46730e14e
David Rosado <dr997@georgetown.edu>
2023-10-18 15:39:00 -0400

    parametrizing and adding async to tests

--- test/test_geonames_api.py
+++ test/test_geonames_api.py
@@ -3,24 +3,38 @@ from network.geo_api import GeonamesApi
 
 
 @pytest.fixture(scope="module")
-def set_up_geonames_api():
+def geoapi():
     geonames_api = GeonamesApi()
     return geonames_api
 
 
-@pytest.mark.geonames("name, final", ("France", "seglns"), (3017382, None))
-async def test_search_geoname_id(geo_api, name, final):
-    results = await geo_api.search_geonames_id(name)
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("name, final", [("France", 3017382), ("Turkey", 298795)])
+async def test_success_search_geoname_id(geoapi, name, final):
+    results = await geoapi.search_geoname_id(name)
     assert results.get("geonameId") == final
 
 
-@pytest.mark.geonames("id, final")
-async def test_search_hierarchy(geo_api, geoid, final):
-    results = await geo_api.search_hierarchy(geoid)
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("name, final", [("afkbsfkb", None), ("3248fn", None)])
+async def test_fail_search_geoname_id(geoapi, name, final):
+    results = await geoapi.search_geoname_id(name)
+    assert results == final
+
+
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("id, final")
+async def test_search_hierarchy(geoapi, geoid, final):
+    results = await geoapi.search_hierarchy(geoid)
     assert results.get("geonameId") == final
 
 
-@pytest.mark.geonames("location, final")
-async def test_search_lat_long(geo_api, location, final):
-    results = await geo_api.search_lat_long(location)
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("location, final")
+async def test_search_lat_long(geoapi, location, final):
+    results = await geoapi.search_lat_long(location)
     assert results.get("geonameId") == final
