206e55ed33f4d36d983d1198ef348c1a0a83896e
David Rosado <dr997@georgetown.edu>
2023-10-18 10:33:43 -0400

    removing old implementation gmpd

--- gmpd/get_rows.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def get_rows():
-    rows = []
-    with open("gmpd/data/GMPD_main.csv", encoding='utf-8') as gmpd_file:
-        rows = list(csv.DictReader(gmpd_file))
-
-
-    return rows
