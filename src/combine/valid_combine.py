a641ed6a92c8e9b08482c3e12204255b483cdd37
David Rosado <dr997@georgetown.edu>
2023-10-18 11:28:41 -0400

    return preprocessed combine rows

--- src/combine/valid_combine.py
+++ src/combine/valid_combine.py
@@ -19,26 +19,19 @@ def log_taxons_without_match(result: list, row: dict) -> None:
         logger.warning(row["iucn2020_binomial"] + " doesn't have a match")
 
 
-def ingest_combine(session) -> None:
+def valid_combine() -> list:
+    combine_valid = []
     with open("data/combine_trait_data_imputed.csv", "r", encoding="utf-8") as combine:
         header = next(combine).replace("-", "_").strip().split(",")
         for line in combine:
             line = line.strip()
             row_data = re.split(r"(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", line)
             row_data = [data for data in row_data if data != ""]
-            data = {key: value for key, value in zip(header, row_data)}
-            data["biogeographical_realm"] = preprocess_biogeographical_realms(
+            data = dict(zip(header, row_data))
+            combine_row = extract_properties(data)
+            combine_row["biogeographical_realm"] = preprocess_biogeographical_realms(
                 data["biogeographical_realm"]
             )
-            properties = extract_properties(data)
-            for realm in data["biogeographical_realm"]:
-                add_properties_query = f"""
-                    MATCH (t:Taxon)
-                    WHERE t.name = "{data['iucn2020_binomial']}"
-                    SET t += {{ {create_properties(properties)} }}
-                    MERGE (g:BioGeographicalRealm:Geography {{name : "{realm}" }})
-                    MERGE (t)-[:INHABITS]->(g)
-                    RETURN t
-                """
-                result = session.run(add_properties_query, properties)
-                log_taxons_without_match(list(result), data)
+            combine_valid.append(combine_row)
+
+    return combine_valid
