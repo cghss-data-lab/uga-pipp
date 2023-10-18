376810662a9c81b88f666a0b78f6f400129e558d
David Rosado <dr997@georgetown.edu>
2023-10-18 15:10:37 -0400

    testing geonames api methods initial

--- test/test_geonames_api.py
+++ test/test_geonames_api.py
@@ -8,6 +8,19 @@ def set_up_geonames_api():
     return geonames_api
 
 
-@pytest.mark.geonames
-def test_search_id(geonames_api):
-    pass
+@pytest.mark.geonames("name, final", ("France", "seglns"), (3017382, None))
+async def test_search_geoname_id(geo_api, name, final):
+    results = await geo_api.search_geonames_id(name)
+    assert results.get("geonameId") == final
+
+
+@pytest.mark.geonames("id, final")
+async def test_search_hierarchy(geo_api, geoid, final):
+    results = await geo_api.search_hierarchy(geoid)
+    assert results.get("geonameId") == final
+
+
+@pytest.mark.geonames("location, final")
+async def test_search_lat_long(geo_api, location, final):
+    results = await geo_api.search_lat_long(location)
+    assert results.get("geonameId") == final
