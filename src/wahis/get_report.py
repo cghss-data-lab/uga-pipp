bb953281863fec318cc5084dbf67cab09b1b806f
David Rosado <dr997@georgetown.edu>
2023-10-19 12:52:11 -0400

    moving wahis to src

--- src/wahis/get_report.py
+++ src/wahis/get_report.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def get_report(reportId):
     report_url = f"https://wahis.woah.org/api/v1/pi/review/report/{reportId}/all-information?language=en"
     report_response = requests.get(report_url)
