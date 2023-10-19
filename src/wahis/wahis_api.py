c5ea81aa1473ceeda9abe5148d53b999990e8a70
David Rosado <dr997@georgetown.edu>
2023-10-19 13:04:59 -0400

    variables with snake naming convention

--- src/wahis/wahis_api.py
+++ src/wahis/wahis_api.py
@@ -2,8 +2,8 @@ import aiohttp
 
 
 class WAHISApi:
-    async def search_evolution(self, eventId):
-        evolution_url = f"event/{eventId}/report-evolution?language=en"
+    async def search_evolution(self, event_id):
+        evolution_url = f"event/{event_id}/report-evolution?language=en"
         data = await self._wahis_api(evolution_url)
         evolution_list = []
         if data:
@@ -14,14 +14,14 @@ class WAHISApi:
             return evolution_list
         return None
 
-    async def search_outbreak(self, reportId, eventId):
+    async def search_outbreak(self, report_id, event_id):
         outbreak_url = (
-            f"review/report/{reportId}/outbreak/{eventId}/all-information?language=en"
+            f"review/report/{report_id}/outbreak/{event_id}/all-information?language=en"
         )
         return await self._wahis_api(outbreak_url)
 
-    async def search_report(self, reportId):
-        report_url = f"review/report/{reportId}/all-information?language=en"
+    async def search_report(self, report_id):
+        report_url = f"review/report/{report_id}/all-information?language=en"
         return await self._wahis_api(report_url)
 
     async def _wahis_api(self, url) -> dict:
