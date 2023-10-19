9e0522bb494812f5460728d4f7b58cf9e3add1ec
David Rosado <dr997@georgetown.edu>
2023-10-19 13:02:28 -0400

    wahis api as async

--- src/wahis/wahis_api.py
+++ src/wahis/wahis_api.py
@@ -1,11 +1,10 @@
-import requests
+import aiohttp
 
 
-class WAHIS:
-    def get_evolution(self, eventId):
-        evolution_url = f"https://wahis.woah.org/api/v1/pi/event/{eventId}/report-evolution?language=en"
-        evolution_response = requests.get(evolution_url)
-        data = evolution_response.json()
+class WAHISApi:
+    async def search_evolution(self, eventId):
+        evolution_url = f"event/{eventId}/report-evolution?language=en"
+        data = await self._wahis_api.get(evolution_url)
         evolution_list = []
         if data:
             for key in data:
@@ -16,14 +15,17 @@ class WAHIS:
         else:
             return None
 
-    def get_outbreak(self, reportId, eventId):
-        outbreak_url = f"https://wahis.woah.org/api/v1/pi/review/report/{reportId}/outbreak/{eventId}/all-information?language=en"
-        outbreak_response = requests.get(outbreak_url)
-        data = outbreak_response.json()
-        return data
+    async def search_outbreak(self, reportId, eventId):
+        outbreak_url = f"review/report/{reportId}/outbreak/{eventId}/all-information?language=en"
+        return await self._wahis_api.get(outbreak_url)
 
-    def get_report(self, reportId):
-        report_url = f"https://wahis.woah.org/api/v1/pi/review/report/{reportId}/all-information?language=en"
-        report_response = requests.get(report_url)
-        data = report_response.json()
-        return data
+    async def search_report(self, reportId):
+        report_url = f"review/report/{reportId}/all-information?language=en"
+        return await self._wahis_api.get(report_url)
+        
+
+    async def _wahis_api(self, url) -> dict:
+        base_url = f"https://wahis.woah.org/api/v1/pi/{url}
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            async with session.get(base_url) as response:
+                return response.json()
