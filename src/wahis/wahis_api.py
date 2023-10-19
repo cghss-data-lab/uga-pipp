ec5bebfb3d827369394737a3b02145837422f693
David Rosado <dr997@georgetown.edu>
2023-10-19 13:03:55 -0400

    missing " and removing get from method

--- src/wahis/wahis_api.py
+++ src/wahis/wahis_api.py
@@ -4,7 +4,7 @@ import aiohttp
 class WAHISApi:
     async def search_evolution(self, eventId):
         evolution_url = f"event/{eventId}/report-evolution?language=en"
-        data = await self._wahis_api.get(evolution_url)
+        data = await self._wahis_api(evolution_url)
         evolution_list = []
         if data:
             for key in data:
@@ -12,20 +12,20 @@ class WAHISApi:
                 evolution_list.append(reportId)
 
             return evolution_list
-        else:
-            return None
+        return None
 
     async def search_outbreak(self, reportId, eventId):
-        outbreak_url = f"review/report/{reportId}/outbreak/{eventId}/all-information?language=en"
-        return await self._wahis_api.get(outbreak_url)
+        outbreak_url = (
+            f"review/report/{reportId}/outbreak/{eventId}/all-information?language=en"
+        )
+        return await self._wahis_api(outbreak_url)
 
     async def search_report(self, reportId):
         report_url = f"review/report/{reportId}/all-information?language=en"
-        return await self._wahis_api.get(report_url)
-        
+        return await self._wahis_api(report_url)
 
     async def _wahis_api(self, url) -> dict:
-        base_url = f"https://wahis.woah.org/api/v1/pi/{url}
+        base_url = f"https://wahis.woah.org/api/v1/pi/{url}"
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(base_url) as response:
                 return response.json()
