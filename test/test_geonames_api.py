0d615dc3eef8919a63342cf395e6ee0576d7c111
David Rosado <dr997@georgetown.edu>
2023-10-19 10:39:01 -0400

    testing geoapi methods and errors

--- test/test_geonames_api.py
+++ test/test_geonames_api.py
@@ -1,5 +1,5 @@
 import pytest
-from network.geo_api import GeonamesApi
+from network.geo_api import GeonamesApi, GeonamesApiError
 
 
 @pytest.fixture(scope="module")
@@ -19,22 +19,22 @@ async def test_success_search_geoname_id(geoapi, name, final):
 @pytest.mark.asyncio
 @pytest.mark.geonames
 @pytest.mark.parametrize("name, final", [("afkbsfkb", None), ("3248fn", None)])
-async def test_fail_search_geoname_id(geoapi, name, final):
+async def test_fails_search_geoname_id(geoapi, name, final):
     results = await geoapi.search_geoname_id(name)
     assert results == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.geonames
-@pytest.mark.parametrize("id, final")
-async def test_search_hierarchy(geoapi, geoid, final):
+@pytest.mark.parametrize("geoid, final", [(3017382, 3), (298795, 3), (7733353, 5)])
+async def test_success_search_hierarchy(geoapi, geoid, final):
     results = await geoapi.search_hierarchy(geoid)
-    assert results.get("geonameId") == final
+    assert len(results) == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.geonames
-@pytest.mark.parametrize("location, final")
-async def test_search_lat_long(geoapi, location, final):
-    results = await geoapi.search_lat_long(location)
-    assert results.get("geonameId") == final
+@pytest.mark.parametrize("geoid", [("3248fn",)])
+async def test_fail_search_hierarchy(geoapi, geoid):
+    with pytest.raises(GeonamesApiError):
+        await geoapi.search_hierarchy(geoid)
