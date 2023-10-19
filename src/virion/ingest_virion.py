72732f00e288cb93ba711bbb866e3754951e69b7
David Rosado <dr997@georgetown.edu>
2023-10-19 12:35:25 -0400

    ingesting virion

--- src/virion/ingest_virion.py
+++ src/virion/ingest_virion.py
@@ -3,6 +3,22 @@ from network.handle_concurrency import handle_concurrency
 from virion.valid_virion import valid_virion
 
 
-async def ingest_virion(session):
-    with open("virion/data/Virion.csv", "r", encoding="utf-8") as virion:
-        virion = valid_virion()
+async def ingest_virion(
+    database_handler, ncbiapi, batch_size=1000, query_path="src/virion/virion.cypher"
+) -> None:
+    virion, ncbi_hierarchies = valid_virion(ncbiapi)
+
+    ncbi_hierarchies = await handle_concurrency(*ncbi_hierarchies)
+
+    batches = (len(virion) - 1) // batch_size + 1
+    for i in range(batches):
+        batch = virion[i * batch_size : (i + 1) * batch_size]
+        await database_handler.execute_query(query_path, properties=batch)
+
+    ncbi = [ncbi for ncbi in ncbi_hierarchies if ncbi is not None]
+    await handle_concurrency(
+        *[
+            database_handler.build_ncbi_hierarchy(hierarchy)
+            for hierarchy in ncbi_hierarchies
+        ]
+    )
