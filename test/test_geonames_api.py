6c44d663aa710405740b1fc94c4f6db3b376e1b5
David Rosado <dr997@georgetown.edu>
2023-10-19 10:50:08 -0400

    adding more geonames tests

--- test/test_geonames_api.py
+++ test/test_geonames_api.py
@@ -38,3 +38,29 @@ async def test_success_search_hierarchy(geoapi, geoid, final):
 async def test_fail_search_hierarchy(geoapi, geoid):
     with pytest.raises(GeonamesApiError):
         await geoapi.search_hierarchy(geoid)
+
+
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize(
+    "location, final", [((1.32, -65.21), 8458367), ((1.32, -65.21), 8458367)]
+)
+async def test_success_search_lat_long(geoapi, location, final):
+    results = await geoapi.search_lat_long(location)
+    assert results.get("geonameId") == final
+
+
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("location, final", [((-74.21, 34.65), None)])
+async def test_fail_search_lat_long(geoapi, location, final):
+    results = await geoapi.search_lat_long(location)
+    assert results == final
+
+
+@pytest.mark.asyncio
+@pytest.mark.geonames
+@pytest.mark.parametrize("location", [(-2000.21, 484.65)])
+async def test_error_search_lat_long(geoapi, location):
+    with pytest.raises(GeonamesApiError):
+        await geoapi.search_lat_long(location)
