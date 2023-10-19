fd972958a8b8e06a363d394a09443a54e87ebdee
David Rosado <dr997@georgetown.edu>
2023-10-19 10:57:15 -0400

    moving txt content to pop api file

--- src/worldpop/pop_api.py
+++ src/worldpop/pop_api.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# LANDING: https://population.un.org/wpp/Download/Standard/CSV/
+# LINK: https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2022_Demographic_Indicators_Medium.zip
+
 
 # TO DO: Doesn't seem to be publicly advertised, but found this.
 # Can't figure out how to search by location name and return ID
