a0b1915679f38e5a97e04c6bb5dfbb553b2b45a4
David Rosado <dr997@georgetown.edu>
2023-10-19 11:47:56 -0400

    processing geographies in worldpop

--- src/worldpop/ingest_worldpop.py
+++ src/worldpop/ingest_worldpop.py
@@ -8,7 +8,7 @@ async def ingest_worldpop(
 ) -> None:
     worldpop, iso_codes, geoids = valid_worldpop(geoapi)
 
-    geoids = await handle_concurrency(geoids)
+    geoids = await handle_concurrency(*geoids)
     isos = dict(zip(iso_codes, geoids))
 
     for row in worldpop:
@@ -18,3 +18,15 @@ async def ingest_worldpop(
     for i in range(batches):
         batch = worldpop[i * batch_size : (i + 1) * batch_size]
         await database_handler.execute_query(query_path, properties=batch)
+
+    geoids = [geoid for geoid in geoids if geoid is not None]
+    geo_hierarchies = await handle_concurrency(
+        *[geoapi.search_hierarchy(geoid) for geoid in geoids]
+    )
+
+    await handle_concurrency(
+        *[
+            database_handler.build_geohierarchy(hierarchy)
+            for hierarchy in geo_hierarchies
+        ]
+    )
