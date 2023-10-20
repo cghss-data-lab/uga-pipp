43eec6cb9357cba735e76e3c8cf60e28bda967b4
David Rosado <dr997@georgetown.edu>
2023-10-20 15:46:15 -0400

    cache tests

--- test/test_cache.py
+++ test/test_cache.py
@@ -1,16 +1,38 @@
-import pickle
+import os
 import pytest
-from cache.cache import cache
+from cache.cache import cache, save_cache, load_cache
+
+TEST_CACHE_FILE = "test/test.pickle"
 
 
-@pytest.fixture(scope="module")
-@cache("test/test.pickle")
-def mock_function(n):
+@cache(TEST_CACHE_FILE)
+async def mock_function(n):
     return n + 1
 
 
 @pytest.mark.asyncio
 @pytest.mark.cache
-@pytest.mark.parametrize("", [])
-async def test_cache(mock_function):
-    pass
+async def test_save_cache():
+    await save_cache({}, TEST_CACHE_FILE)
+    assert os.path.isfile(TEST_CACHE_FILE)
+
+
+@pytest.mark.asyncio
+@pytest.mark.cache
+def test_load_cache():
+    memoize = load_cache(TEST_CACHE_FILE)
+    assert memoize == {}
+
+
+@pytest.mark.asyncio
+@pytest.mark.cache
+@pytest.mark.parametrize("n, final", [(1, 2), (90, 91), (10, 11), (4, 5)])
+async def test_cache(n, final):
+    await mock_function(n)
+    assert mock_function.cache[n] == final
+
+
+# Not a test, but deletes the mock cache file
+@pytest.mark.cache
+def test_delete():
+    os.remove(TEST_CACHE_FILE)
