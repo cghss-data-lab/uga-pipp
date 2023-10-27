c409954464d76c6e572047614894ccf657a7b243
David Rosado <dr997@georgetown.edu>
2023-10-27 11:04:27 -0400

    adding ncbi tests

--- test/test_ncbi_api.py
+++ test/test_ncbi_api.py
@@ -1,5 +1,5 @@
 import pytest
-from network.ncbi_api import NCBIApi
+from network.ncbi_api import NCBIApi, NCBIApiError
 
 
 @pytest.fixture(scope="module")
@@ -10,7 +10,7 @@ def ncbiapi():
 
 @pytest.mark.asyncio
 @pytest.mark.ncbi
-@pytest.mark.parametrize("name, final", [()])
+@pytest.mark.parametrize("name, final", [("Blastocerus dichotomus", 248133)])
 async def test_success_search_id(ncbiapi, name, final):
     result = await ncbiapi.search_id(name)
     assert result == final
@@ -18,7 +18,7 @@ async def test_success_search_id(ncbiapi, name, final):
 
 @pytest.mark.asyncio
 @pytest.mark.ncbi
-@pytest.mark.parametrize("name, final", [()])
+@pytest.mark.parametrize("name, final", [("fklgsd", None)])
 async def test_fail_search_id(ncbiapi, name, final):
     result = await ncbiapi.search_id(name)
     assert result == final
@@ -26,15 +26,24 @@ async def test_fail_search_id(ncbiapi, name, final):
 
 @pytest.mark.asyncio
 @pytest.mark.ncbi
-@pytest.mark.parametrize("taxid, final", [()])
+@pytest.mark.parametrize("taxid, final", [(9606, 31)])
 async def test_success_search_hierarchy(ncbiapi, taxid, final):
+    result = await ncbiapi.search_id(taxid)
+    assert isinstance(result, list)
+    assert len(result) == final
+
+
+@pytest.mark.asyncio
+@pytest.mark.ncbi
+@pytest.mark.parametrize("taxid, final", [(96394106, None)])
+async def test_fail_search_hierarchy(ncbiapi, taxid, final):
     result = await ncbiapi.search_id(taxid)
     assert result == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.ncbi
-@pytest.mark.parametrize("taxid, final", [()])
-async def test_fail_search_hierarchy(ncbiapi, taxid, final):
-    result = await ncbiapi.search_id(taxid)
-    assert result == final
+@pytest.mark.parametrize("taxid", [("s;dflmwg",)])
+async def test_ncbi_error(ncbiapi, taxid):
+    with pytest.raises(NCBIApiError):
+        await ncbiapi.search_id(taxid)
