ce8f0f775cfd1fdc8944327917b44e10e6abc080
David Rosado <dr997@georgetown.edu>
2023-11-06 13:30:40 -0500

    moving to test folder

--- test/timer.py
+++ test/timer.py
@@ -5,10 +5,10 @@ from loguru import logger
 
 def timer(function):
     @wraps(function)
-    def quantize(*args):
-        start = time.time()
-        result = function(*args)
-        end = time.time()
+    async def quantize(*args, **kwargs):
+        start = time.monotonic()
+        result = await function(*args, **kwargs)
+        end = time.monotonic()
 
         logger.debug(f"Function: {function} Seconds: {end - start}")
         return result
