d0b0967429e920fdeae2976d35f45397d5f5fa12
David Rosado <dr997@georgetown.edu>
2023-11-07 16:40:38 -0500

    updating cache and adding some wahis tests

--- tests/test_wahis_api.py
+++ tests/test_wahis_api.py
@@ -1,5 +1,5 @@
 import pytest
-from src.wahis.wahis_api import WAHISApi
+from src.wahis.wahis_api import WAHISApi, WAHISApiError
 
 
 @pytest.fixture(scope="module")
@@ -10,27 +10,39 @@ def wahisapi():
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
-@pytest.mark.parametrize("event_id, final", [()])
-def test_success_search_evolution(wahisapi):
-    pass
+@pytest.mark.parametrize("event_id, final", [(4714, 3), (32, 2)])
+async def test_success_search_evolution(wahisapi, event_id, final):
+    result = await wahisapi.search_evolution(event_id)
+    assert len(result) == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
-@pytest.mark.parametrize("event_id, final", [()])
-def test_fail_search_evolution(wahisapi):
-    pass
+@pytest.mark.parametrize("event_id, final", [(-23, None), (0, None)])
+async def test_fail_search_evolution(wahisapi, event_id, final):
+    result = await wahisapi.search_evolution(event_id)
+    assert result == final
+
+
+@pytest.mark.asyncio
+@pytest.mark.wahis
+@pytest.mark.parametrize("event_id", [("soymilk"), ("evolution")])
+async def test_error_search_evolution(wahisapi, event_id):
+    with pytest.raises(WAHISApiError):
+        await wahisapi.search_evolution(event_id)
 
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
 @pytest.mark.parametrize("report_id, final", [()])
-def test_success_search_report(wahisapi):
-    pass
+async def test_success_search_report(wahisapi, report_id, final):
+    result = await wahisapi.search_report(report_id)
+    assert result == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
 @pytest.mark.parametrize("report_id, final", [()])
-def test_fail_search_report(wahisapi):
-    pass
+async def test_fail_search_report(wahisapi, report_id, final):
+    result = await wahisapi.search_report(report_id)
+    assert result == final
