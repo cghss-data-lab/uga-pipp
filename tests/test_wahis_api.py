961315febc41d4544ea97db0073b799dd2b95779
David Rosado <dr997@georgetown.edu>
2023-11-07 17:01:00 -0500

    more wahis tests

--- tests/test_wahis_api.py
+++ tests/test_wahis_api.py
@@ -34,15 +34,26 @@ async def test_error_search_evolution(wahisapi, event_id):
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
-@pytest.mark.parametrize("report_id, final", [()])
-async def test_success_search_report(wahisapi, report_id, final):
+@pytest.mark.parametrize("report_id, country", [(157894, "Germany"), (1574, "Spain")])
+async def test_success_search_report(wahisapi, report_id, country):
+    result = await wahisapi.search_report(report_id)
+    assert (result["report"]["reportId"], result["event"]["country"]["name"]) == (
+        report_id,
+        country,
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.wahis
+@pytest.mark.parametrize("report_id, final", [(-23, None), (0, None)])
+async def test_fail_search_report(wahisapi, report_id, final):
     result = await wahisapi.search_report(report_id)
     assert result == final
 
 
 @pytest.mark.asyncio
 @pytest.mark.wahis
-@pytest.mark.parametrize("report_id, final", [()])
-async def test_fail_search_report(wahisapi, report_id, final):
-    result = await wahisapi.search_report(report_id)
-    assert result == final
+@pytest.mark.parametrize("report_id", [("sandwich"), ("13fk94"), ("894$1")])
+async def test_error_search_report(wahisapi, report_id):
+    with pytest.raises(WAHISApiError):
+        await wahisapi.search_report(report_id)
