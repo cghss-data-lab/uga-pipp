c4d38827b94fbe79e0d82fd57772dc86e6b12c1a
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-02-13 11:52:42 -0300

    comment out bvbrc ingest

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from loguru import logger
 from network.neo4j_handler import Neo4jHandler
 from network.geo_api import GeonamesApi
 from network.ncbi_api import NCBIApi
+
 from src.flunet.ingest_flunet import ingest_flunet
 from src.gmpd.ingest_gmpd import ingest_gmpd
 from src.wahis.ingest_wahis import ingest_wahis
 from src.combine.ingest_combine import ingest_combine
 from src.virion.ingest_virion import ingest_virion
 from src.worldpop.ingest_worldpop import ingest_worldpop
+
+# from src.bvbrc_surveillance.ingest_bvbrc_surveillance import ingest_bvbrc_surveillance
 
 
 logger.remove(0)
@@ -19,10 +22,18 @@ logger.add(sys.stderr, level="TRACE")
 async def main() -> None:
     database_handler, geonames_api, ncbi_api = Neo4jHandler(), GeonamesApi(), NCBIApi()
 
+    # with open("network/cache/ncbi_id.pickle", "rb") as c:
+    #     ncbi_cache = pickle.load(c)
+    #     for key, value in ncbi_cache.items():
+    #         if value is None:
+    #             print(key)
+
     await ingest_wahis(database_handler, geonames_api, ncbi_api)
     await ingest_virion(database_handler, ncbi_api)
     await ingest_flunet(database_handler, geonames_api, ncbi_api)
     await ingest_gmpd(database_handler, geonames_api, ncbi_api)
+
+    # await ingest_bvbrc_surveillance(database_handler, geonames_api, ncbi_api)
 
     # Keep combine and worldpop at the end of ingestion
     await ingest_combine(database_handler)
