9f6211ca7edf88f9c4e06453d82fb63dab3515ee
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2024-07-18 14:26:25 -0400

    Remove virion

--- src/carnivoreGMPD/get_rows.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def get_rows():
-    rows = []
-    with open("carnivoreGMPD/data/carnivoreGMPD.csv") as pairings_file:
-        rows = list(csv.DictReader(pairings_file))
-
-    return rows
